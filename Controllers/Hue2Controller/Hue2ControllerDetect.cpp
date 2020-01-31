779471e3e40ad2ff2a106b3b8c150a58434f8b9e
Adam Honse <calcprogrammer1@gmail.com>
2020-01-31 11:08:33 -0600

    Detect NZXT Smart Device V2 and use the Hue 2 driver for it

--- Controllers/Hue2Controller/Hue2ControllerDetect.cpp
+++ Controllers/Hue2Controller/Hue2ControllerDetect.cpp
@@ -6,6 +6,7 @@
 
 #define NZXT_HUE_2_VID 0x1E71
 #define NZXT_HUE_2_PID 0x2001
+#define NZXT_SMART_DEVICE_V2_PID 0x2006
 
 /******************************************************************************************\
 *                                                                                          *
@@ -21,14 +22,29 @@ void DetectHue2Controllers(std::vector<RGBController*> &rgb_controllers)
     libusb_init(&ctx);
 
     //Look for NZXT Hue 2
-    libusb_device_handle * dev = libusb_open_device_with_vid_pid(ctx, NZXT_HUE_2_VID, NZXT_HUE_2_PID);
+    libusb_device_handle * hue_2_dev = libusb_open_device_with_vid_pid(ctx, NZXT_HUE_2_VID, NZXT_HUE_2_PID);
 
-    if( dev )
+    if( hue_2_dev )
     {
-        libusb_detach_kernel_driver(dev, 0);
-        libusb_claim_interface(dev, 0);
+        libusb_detach_kernel_driver(hue_2_dev, 0);
+        libusb_claim_interface(hue_2_dev, 0);
 
-        Hue2Controller* controller = new Hue2Controller(dev);
+        Hue2Controller* controller = new Hue2Controller(hue_2_dev);
+
+        RGBController_Hue2* rgb_controller = new RGBController_Hue2(controller);
+
+        rgb_controllers.push_back(rgb_controller);
+    }
+
+    //Look for NZXT Smart Device V2
+    libusb_device_handle * smart_dev_2_dev = libusb_open_device_with_vid_pid(ctx, NZXT_HUE_2_VID, NZXT_SMART_DEVICE_V2_PID);
+
+    if( smart_dev_2_dev )
+    {
+        libusb_detach_kernel_driver(smart_dev_2_dev, 0);
+        libusb_claim_interface(smart_dev_2_dev, 0);
+
+        Hue2Controller* controller = new Hue2Controller(smart_dev_2_dev);
 
         RGBController_Hue2* rgb_controller = new RGBController_Hue2(controller);
 
