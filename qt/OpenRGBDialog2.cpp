d330dc03445b8c4ff365c8d63c0d70339daddef4
Adam Honse <calcprogrammer1@gmail.com>
2020-02-26 17:37:56 -0600

    Add dialog to name profile.  Save all controllers to a single file.  Add a file header with version number and use extension .orp for OpenRgb Profile

--- qt/OpenRGBDialog2.cpp
+++ qt/OpenRGBDialog2.cpp
@@ -3,6 +3,7 @@
 #include "OpenRGBDevicePage.h"
 #include "OpenRGBDeviceInfoPage.h"
 #include "OpenRGBSystemInfoPage.h"
+#include "OpenRGBProfileSaveDialog.h"
 #include "RGBController_Dummy.h"
 #include <QLabel>
 #include <QTabBar>
@@ -282,100 +283,171 @@ void OpenRGBDialog2::on_ShowHide()
 
 void Ui::OpenRGBDialog2::on_ButtonSaveProfile_clicked()
 {
-    for(int controller_index = 0; controller_index < controllers.size(); controller_index++)
-    {
-        char filename[128];
-        snprintf(filename, 128, "controller_%d.bin", controller_index);
+    OpenRGBProfileSaveDialog dialog;
 
+    /*---------------------------------------------------------*\
+    | Open Profile Name Dialog                                  |
+    \*---------------------------------------------------------*/
+    std::string profile_name = dialog.show();
+
+    /*---------------------------------------------------------*\
+    | If a name was entered, save the profile file              |
+    \*---------------------------------------------------------*/
+    if(profile_name != "")
+    {
+        /*---------------------------------------------------------*\
+        | Extension .orp - OpenRgb Profile                          |
+        \*---------------------------------------------------------*/
+        std::string filename = profile_name + ".orp";
+
+        /*---------------------------------------------------------*\
+        | Open an output file in binary mode                        |
+        \*---------------------------------------------------------*/
         std::ofstream controller_file(filename, std::ios::out | std::ios::binary);
 
-        unsigned char *controller_data = controllers[controller_index]->GetDeviceDescription();
-        unsigned int controller_size;
+        /*---------------------------------------------------------*\
+        | Write header                                              |
+        | 16 bytes - "OPENRGB_PROFILE"                              |
+        | 4 bytes - Version, unsigned int                           |
+        \*---------------------------------------------------------*/
+        unsigned int profile_version = 1;
+        controller_file.write("OPENRGB_PROFILE", 16);
+        controller_file.write((char *)&profile_version, sizeof(unsigned int));
 
-        memcpy(&controller_size, controller_data, sizeof(controller_size));
+        /*---------------------------------------------------------*\
+        | Write controller data for each controller                 |
+        \*---------------------------------------------------------*/
+        for(int controller_index = 0; controller_index < controllers.size(); controller_index++)
+        {
+            unsigned char *controller_data = controllers[controller_index]->GetDeviceDescription();
+            unsigned int controller_size;
 
-        controller_file.write((const char *)controller_data, controller_size);
+            memcpy(&controller_size, controller_data, sizeof(controller_size));
 
+            controller_file.write((const char *)controller_data, controller_size);
+        }
+
+        /*---------------------------------------------------------*\
+        | Close the file when done                                  |
+        \*---------------------------------------------------------*/
         controller_file.close();
     }
 }
 
 void Ui::OpenRGBDialog2::on_ButtonLoadProfile_clicked()
 {
-    for(int controller_index = 0; controller_index < controllers.size(); controller_index++)
+    std::vector<RGBController*> temp_controllers;
+    unsigned int controller_size;
+    unsigned int controller_offset = 0;
+
+    /*---------------------------------------------------------*\
+    | Get the profile filename from the profiles list           |
+    \*---------------------------------------------------------*/
+    std::string filename = "test123.orp";
+
+    /*---------------------------------------------------------*\
+    | Open input file in binary mode                            |
+    \*---------------------------------------------------------*/
+    std::ifstream controller_file(filename, std::ios::in | std::ios::binary);
+
+    /*---------------------------------------------------------*\
+    | Read and verify file header                               |
+    \*---------------------------------------------------------*/
+    char            header_string[16];
+    unsigned int    header_version;
+
+    controller_file.read(header_string, 16);
+    controller_file.read((char *)&header_version, sizeof(unsigned int));
+
+    controller_offset += 16 + sizeof(unsigned int);
+    controller_file.seekg(controller_offset);
+
+    if(strcmp(header_string, "OPENRGB_PROFILE") == 0)
     {
-        char filename[128];
-        snprintf(filename, 128, "controller_%d.bin", controller_index);
-
-        std::ifstream controller_file(filename, std::ios::in | std::ios::binary);
-        unsigned int controller_size;
-
-        controller_file.read((char *)&controller_size, sizeof(controller_size));
-
-        unsigned char *controller_data = new unsigned char[controller_size];
-
-        controller_file.seekg(0);
-
-        controller_file.read((char *)controller_data, controller_size);
-
-        RGBController_Dummy temp_controller;
-
-        temp_controller.ReadDeviceDescription(controller_data);
-
-        delete[] controller_data;
-
-        RGBController *controller_ptr = controllers[controller_index];
-
-        /*---------------------------------------------------------*\
-        | Test if saved controller data matches this controller     |
-        \*---------------------------------------------------------*/
-        if((temp_controller.type        == controller_ptr->type       )
-         &&(temp_controller.name        == controller_ptr->name       )
-         &&(temp_controller.description == controller_ptr->description)
-         &&(temp_controller.version     == controller_ptr->version    )
-         &&(temp_controller.serial      == controller_ptr->serial     )
-         &&(temp_controller.location    == controller_ptr->location   ))
+        if(header_version == 1)
         {
             /*---------------------------------------------------------*\
-            | Update all modes                                          |
+            | Read controller data from file until EOF                  |
             \*---------------------------------------------------------*/
-            if(temp_controller.modes.size() == controller_ptr->modes.size())
+            while(!(controller_file.peek() == EOF))
             {
-                for(int mode_index = 0; mode_index < temp_controller.modes.size(); mode_index++)
-                {
-                    if((temp_controller.modes[mode_index].name       == controller_ptr->modes[mode_index].name      )
-                     &&(temp_controller.modes[mode_index].value      == controller_ptr->modes[mode_index].value     )
-                     &&(temp_controller.modes[mode_index].flags      == controller_ptr->modes[mode_index].flags     )
-                     &&(temp_controller.modes[mode_index].speed_min  == controller_ptr->modes[mode_index].speed_min )
-                     &&(temp_controller.modes[mode_index].speed_max  == controller_ptr->modes[mode_index].speed_max )
-                     &&(temp_controller.modes[mode_index].colors_min == controller_ptr->modes[mode_index].colors_min)
-                     &&(temp_controller.modes[mode_index].colors_max == controller_ptr->modes[mode_index].colors_max))
-                    {
-                        controller_ptr->modes[mode_index].speed      = temp_controller.modes[mode_index].speed;
-                        controller_ptr->modes[mode_index].direction  = temp_controller.modes[mode_index].direction;
-                        controller_ptr->modes[mode_index].color_mode = temp_controller.modes[mode_index].color_mode;
+                controller_file.read((char *)&controller_size, sizeof(controller_size));
 
-                        controller_ptr->modes[mode_index].colors.resize(temp_controller.modes[mode_index].colors.size());
+                unsigned char *controller_data = new unsigned char[controller_size];
 
-                        for(int mode_color_index = 0; mode_color_index < temp_controller.modes[mode_index].colors.size(); mode_color_index++)
-                        {
-                            controller_ptr->modes[mode_index].colors[mode_color_index] = temp_controller.modes[mode_index].colors[mode_color_index];
-                        }
-                    }
+                controller_file.seekg(controller_offset);
 
-                }
+                controller_file.read((char *)controller_data, controller_size);
 
-                controller_ptr->active_mode = temp_controller.active_mode;
+                RGBController_Dummy *temp_controller = new RGBController_Dummy();
+
+                temp_controller->ReadDeviceDescription(controller_data);
+
+                temp_controllers.push_back(temp_controller);
+
+                delete[] controller_data;
+
+                controller_offset += controller_size;
+                controller_file.seekg(controller_offset);
             }
 
-            /*---------------------------------------------------------*\
-            | Update all colors                                         |
-            \*---------------------------------------------------------*/
-            if(temp_controller.colors.size() == controller_ptr->colors.size())
+            for(int controller_index = 0; controller_index < controllers.size(); controller_index++)
             {
-                for(int color_index = 0; color_index < temp_controller.colors.size(); color_index++)
+                RGBController *temp_controller = temp_controllers[controller_index];
+                RGBController *controller_ptr = controllers[controller_index];
+
+                /*---------------------------------------------------------*\
+                | Test if saved controller data matches this controller     |
+                \*---------------------------------------------------------*/
+                if((temp_controller->type        == controller_ptr->type       )
+                 &&(temp_controller->name        == controller_ptr->name       )
+                 &&(temp_controller->description == controller_ptr->description)
+                 &&(temp_controller->version     == controller_ptr->version    )
+                 &&(temp_controller->serial      == controller_ptr->serial     )
+                 &&(temp_controller->location    == controller_ptr->location   ))
                 {
-                    controller_ptr->colors[color_index] = temp_controller.colors[color_index];
+                    /*---------------------------------------------------------*\
+                    | Update all modes                                          |
+                    \*---------------------------------------------------------*/
+                    if(temp_controller->modes.size() == controller_ptr->modes.size())
+                    {
+                        for(int mode_index = 0; mode_index < temp_controller->modes.size(); mode_index++)
+                        {
+                            if((temp_controller->modes[mode_index].name       == controller_ptr->modes[mode_index].name      )
+                             &&(temp_controller->modes[mode_index].value      == controller_ptr->modes[mode_index].value     )
+                             &&(temp_controller->modes[mode_index].flags      == controller_ptr->modes[mode_index].flags     )
+                             &&(temp_controller->modes[mode_index].speed_min  == controller_ptr->modes[mode_index].speed_min )
+                             &&(temp_controller->modes[mode_index].speed_max  == controller_ptr->modes[mode_index].speed_max )
+                             &&(temp_controller->modes[mode_index].colors_min == controller_ptr->modes[mode_index].colors_min)
+                             &&(temp_controller->modes[mode_index].colors_max == controller_ptr->modes[mode_index].colors_max))
+                            {
+                                controller_ptr->modes[mode_index].speed      = temp_controller->modes[mode_index].speed;
+                                controller_ptr->modes[mode_index].direction  = temp_controller->modes[mode_index].direction;
+                                controller_ptr->modes[mode_index].color_mode = temp_controller->modes[mode_index].color_mode;
+
+                                controller_ptr->modes[mode_index].colors.resize(temp_controller->modes[mode_index].colors.size());
+
+                                for(int mode_color_index = 0; mode_color_index < temp_controller->modes[mode_index].colors.size(); mode_color_index++)
+                                {
+                                    controller_ptr->modes[mode_index].colors[mode_color_index] = temp_controller->modes[mode_index].colors[mode_color_index];
+                                }
+                            }
+
+                        }
+
+                        controller_ptr->active_mode = temp_controller->active_mode;
+                    }
+
+                    /*---------------------------------------------------------*\
+                    | Update all colors                                         |
+                    \*---------------------------------------------------------*/
+                    if(temp_controller->colors.size() == controller_ptr->colors.size())
+                    {
+                        for(int color_index = 0; color_index < temp_controller->colors.size(); color_index++)
+                        {
+                            controller_ptr->colors[color_index] = temp_controller->colors[color_index];
+                        }
+                    }
                 }
             }
         }
---
+++ qt/OpenRGBProfileSaveDialog.h
@@ -0,0 +1,26 @@
+#ifndef OPENRGBPROFILESAVEDIALOG_H
+#define OPENRGBPROFILESAVEDIALOG_H
+
+#include <QDialog>
+#include "ui_OpenRGBProfileSaveDialog.h"
+
+namespace Ui
+{
+class OpenRGBProfileSaveDialog;
+}
+
+class Ui::OpenRGBProfileSaveDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit OpenRGBProfileSaveDialog(QWidget *parent = nullptr);
+    ~OpenRGBProfileSaveDialog();
+
+    std::string show();
+
+private:
+    Ui::OpenRGBProfileSaveDialogUi *ui;
+};
+
+#endif // OPENRGBPROFILESAVEDIALOG_H
