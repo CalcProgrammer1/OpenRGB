e2dfcd5b0ff51701fda15c6bb4f600df590fef19
Adam Honse <calcprogrammer1@gmail.com>
2020-04-24 18:22:17 -0500

    Client now requests list of all controllers from server and adds them to the master list

--- NetworkClient.h
+++ NetworkClient.h
@@ -12,12 +12,18 @@ public:
     void        ConnectionThread();
     void        ListenThread();
 
+    void        ProcessReply_ControllerCount(unsigned int data_size, char * data);
+    void        ProcessReply_ControllerData(unsigned int data_size, char * data);
+    
     void        SendRequest_ControllerCount();
     void        SendRequest_ControllerData(unsigned int idx);
-    
+
 protected:
     std::vector<RGBController *>& controllers;
+    std::vector<RGBController *>  server_controllers;
 
 private:
-    net_port    port;
+    net_port        port;
+    bool            server_connected;
+    unsigned int    server_controller_count;
 };
