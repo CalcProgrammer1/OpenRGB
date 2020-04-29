81acc47cd68a889b208c01d32ae0c8ebe3a8be0f
Adam Honse <calcprogrammer1@gmail.com>
2020-04-29 17:05:46 -0500

    Use std::thread for NetworkServer threads

--- NetworkServer.h
+++ NetworkServer.h
@@ -2,6 +2,8 @@
 #include "NetworkProtocol.h"
 #include "net_port.h"
 
+#include <thread>
+
 #pragma once
 
 class NetworkServer
@@ -9,14 +11,17 @@ class NetworkServer
 public:
     NetworkServer(std::vector<RGBController *>& control);
 
-    void        ConnectionThread();
-    void        ListenThread(SOCKET * client_sock);
+    void        ConnectionThreadFunction();
+    void        ListenThreadFunction(SOCKET * client_sock);
 
     void        SendReply_ControllerCount(SOCKET * client_sock);
     void        SendReply_ControllerData(SOCKET * client_sock, unsigned int dev_idx);
 
 protected:
-    std::vector<RGBController *>& controllers;
+    std::vector<RGBController *>&   controllers;
+
+    std::vector<std::thread *>      ListenThreads;
+    std::thread *                   ConnectionThread;
 
 private:
     net_port    port;
