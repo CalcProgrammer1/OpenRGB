de44c57081a6560409dbe384a083b9553675394e
B Horn <b@horn.uk>
2020-05-17 13:51:05 +0100

    Added all the PIDs for the various Rival 300 Mouses

--- Controllers/SteelSeriesController/SteelSeriesControllerDetect.cpp
+++ Controllers/SteelSeriesController/SteelSeriesControllerDetect.cpp
@@ -8,13 +8,18 @@
 /*-----------------------------------------------------*\
 | Mouse product IDs                                     |
 \*-----------------------------------------------------*/
-#define STEELSERIES_RIVAL_VID           0x1038
-#define STEELSERIES_RIVAL_100_PID       0x1702
-#define STEELSERIES_RIVAL_100_DOTA_PID  0x170c
-#define STEELSERIES_RIVAL_105_PID       0x1814
-#define STEELSERIES_RIVAL_110_PID       0x1729
-#define STEELSERIES_RIVAL_300_PID       0x1384
-
+#define STEELSERIES_RIVAL_VID                       0x1038
+#define STEELSERIES_RIVAL_100_PID                   0x1702
+#define STEELSERIES_RIVAL_100_DOTA_PID              0x170c
+#define STEELSERIES_RIVAL_105_PID                   0x1814
+#define STEELSERIES_RIVAL_110_PID                   0x1729
+#define STEELSERIES_RIVAL_300_PID                   0x1384
+#define ACER_PREDATOR_RIVAL_300_PID                 0x1714
+#define STEELSERIES_RIVAL_300_CSGO_PID              0x1394
+#define STEELSERIES_RIVAL_300_CSGO_STM32_PID        0x1716
+#define STEELSERIES_RIVAL_300_CSGO_HYPERBEAST_PID   0x171a
+#define STEELSERIES_RIVAL_300_DOTA_PID              0x1392
+#define STEELSERIES_RIVAL_300_HP_PID                0x1718
 
 typedef struct
 {
@@ -30,14 +35,20 @@ typedef struct
 
 static const steelseries_device device_list[] = 
 {
-    /*-------------------------------------------------------------------------------------------------------------------------------------*\
-    | Mice                                                                                                                                  |
-    \*-------------------------------------------------------------------------------------------------------------------------------------*/
-    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_100_PID,      0,  DEVICE_TYPE_MOUSE,  RIVAL_100,  "SteelSeries Rival 100"                 },
-    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_100_DOTA_PID, 0,  DEVICE_TYPE_MOUSE,  RIVAL_100,  "SteelSeries Rival 100 DotA 2 Edition"  },
-    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_105_PID,      0,  DEVICE_TYPE_MOUSE,  RIVAL_100,  "SteelSeries Rival 105"                 },
-    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_110_PID,      0,  DEVICE_TYPE_MOUSE,  RIVAL_100,  "SteelSeries Rival 110"                 },
-    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_300_PID,      0,  DEVICE_TYPE_MOUSE,  RIVAL_300,  "SteelSeries Rival 300"                 },
+    /*-------------------------------------------------------------------------------------------------------------------------------------------------------------*\
+    | Mice                                                                                                                                                          |
+    \*-------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_100_PID,                  0,  DEVICE_TYPE_MOUSE,  RIVAL_100,  "SteelSeries Rival 100"                             },
+    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_100_DOTA_PID,             0,  DEVICE_TYPE_MOUSE,  RIVAL_100,  "SteelSeries Rival 100 DotA 2 Edition"              },
+    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_105_PID,                  0,  DEVICE_TYPE_MOUSE,  RIVAL_100,  "SteelSeries Rival 105"                             },
+    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_110_PID,                  0,  DEVICE_TYPE_MOUSE,  RIVAL_100,  "SteelSeries Rival 110"                             },
+    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_300_PID,                  0,  DEVICE_TYPE_MOUSE,  RIVAL_300,  "SteelSeries Rival 300"                             },
+    { STEELSERIES_RIVAL_VID,    ACER_PREDATOR_RIVAL_300_PID,                0,  DEVICE_TYPE_MOUSE,  RIVAL_300,  "Acer Predator Gaming Mouse (Rival 300)"            },
+    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_300_CSGO_PID,             0,  DEVICE_TYPE_MOUSE,  RIVAL_300,  "SteelSeries Rival 300 CS:GO Fade Edition"          },
+    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_300_CSGO_STM32_PID,       0,  DEVICE_TYPE_MOUSE,  RIVAL_300,  "SteelSeries Rival 300 CS:GO Fade Edition (stm32)"  },
+    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_300_CSGO_HYPERBEAST_PID,  0,  DEVICE_TYPE_MOUSE,  RIVAL_300,  "SteelSeries Rival 300 CS:GO Hyperbeast Edition"    },
+    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_300_DOTA_PID,             0,  DEVICE_TYPE_MOUSE,  RIVAL_300,  "SteelSeries Rival 300 Dota 2 Edition"              },
+    { STEELSERIES_RIVAL_VID,    STEELSERIES_RIVAL_300_HP_PID,               0,  DEVICE_TYPE_MOUSE,  RIVAL_300,  "SteelSeries Rival 300 HP Omen Edition"             },
 };
 
 /******************************************************************************************\
