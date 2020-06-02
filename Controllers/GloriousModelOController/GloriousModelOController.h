a34d0f93a3beff14e2adbfd0ce54474ffa060069
crashniels <NielsWest@live.de>
2020-06-02 19:49:00 +0200

    Added most modes in

--- Controllers/GloriousModelOController/GloriousModelOController.h
+++ Controllers/GloriousModelOController/GloriousModelOController.h
@@ -15,7 +15,36 @@
 
 enum
 {
-    GLORIOUS_MODE_STATIC = 0x01,
+    GLORIOUS_MODE_OFF = 0x00, //does nothing
+    GLORIOUS_MODE_RAINBOW = 0x01,
+    GLORIOUS_MODE_STATIC = 0x02,
+    GLORIOUS_MODE_SPECTRUM_BREATING = 0x03,
+    GLORIOUS_MODE_CHASE = 0x04,
+    GLORIOUS_MODE_SPECTRUM_CYCLE = 0x05,
+    GLORIOUS_MODE_FLASHING = 0x07,
+    GLORIOUS_MODE_EPILEPSY = 0x08, //not in the official software
+    GLORIOUS_MODE_SLOW_RAINBOW = 0x09,
+    GLORIOUS_MODE_BREATHING = 0x0a,
+};
+
+enum
+{
+    GLORIOUS_SPEED_SLOW = 0x41,
+    GLORIOUS_SPEED_NORMAL = 0x42,
+    GLORIOUS_SPEED_FAST = 0x43,
+};
+
+enum
+{
+    GLORIOUS_DIRECTION_UP = 0x00,
+    GLORIOUS_DIRECTION_DOWN = 0x01,
+};
+
+enum
+{
+    GLORIOUS_MODE_BREATING_SLOW = 0x01,
+    GLORIOUS_MODE_BREATING_NORMAL = 0x02,
+    GLORIOUS_MODE_BREATING_FAST = 0x03,
 };
 
 class GloriousModelOController
@@ -28,10 +57,14 @@ public:
     unsigned int    GetLEDCount();
 
     void            SetLEDColor(unsigned char red, unsigned char green, unsigned char blue);
-    void            SetMode(unsigned char mode);
+    void            SetMode(unsigned char mode, unsigned char speed, unsigned char direction);
 private:
     libusb_device_handle*   dev;
 
     char                    device_name[32];
     unsigned int            led_count;
+
+    unsigned char           current_mode;
+    unsigned char           current_speed;
+    unsigned char           current_direction;
 };
--- RGBController/RGBController_GloriousModelO.cpp
+++ RGBController/RGBController_GloriousModelO.cpp
@@ -21,7 +21,92 @@ RGBController_GloriousModelO::RGBController_GloriousModelO(GloriousModelOControl
     Static.name       = "Static";
     Static.flags      = MODE_FLAG_HAS_PER_LED_COLOR;
     Static.color_mode = MODE_COLORS_PER_LED;
+    Static.value      = GLORIOUS_MODE_STATIC;
     modes.push_back(Static);
+
+    mode Off;
+    Off.name       = "Off";
+    Off.flags      = 0;
+    Off.color_mode = MODE_COLORS_NONE;
+    Off.value      = GLORIOUS_MODE_OFF;
+    modes.push_back(Off);
+
+    mode Rainbow;
+    Rainbow.name       = "Glorious Mode";
+    Rainbow.flags      = MODE_FLAG_HAS_SPEED | MODE_FLAG_HAS_DIRECTION_UD;
+    Rainbow.speed_min  = GLORIOUS_SPEED_SLOW;
+    Rainbow.speed      = GLORIOUS_SPEED_NORMAL;
+    Rainbow.speed_max  = GLORIOUS_SPEED_FAST;
+    Rainbow.direction  = MODE_DIRECTION_UP;
+    Rainbow.color_mode = MODE_COLORS_NONE;
+    Rainbow.value      = GLORIOUS_MODE_RAINBOW;
+    modes.push_back(Rainbow);
+
+    mode SpectrumBreathing;
+    SpectrumBreathing.name       = "Seemless Breathing";
+    SpectrumBreathing.flags      = MODE_FLAG_HAS_SPEED;
+    SpectrumBreathing.speed_min  = GLORIOUS_SPEED_SLOW;
+    SpectrumBreathing.speed      = GLORIOUS_SPEED_NORMAL;
+    SpectrumBreathing.speed_max  = GLORIOUS_SPEED_FAST;
+    SpectrumBreathing.value      = GLORIOUS_MODE_SPECTRUM_BREATING;
+    modes.push_back(SpectrumBreathing);
+
+    mode Chase;
+    Chase.name       = "Chase";
+    Chase.flags      = MODE_FLAG_HAS_SPEED;
+    Chase.speed_min  = GLORIOUS_SPEED_SLOW;
+    Chase.speed      = GLORIOUS_SPEED_NORMAL;
+    Chase.speed_max  = GLORIOUS_SPEED_FAST;
+    Chase.color_mode = MODE_COLORS_NONE;
+    Chase.value      = GLORIOUS_MODE_CHASE;
+    modes.push_back(Chase);
+
+    mode SpectrumCycle;
+    SpectrumCycle.name       = "Spectrum Cycle";
+    SpectrumCycle.flags      = MODE_FLAG_HAS_SPEED;
+    SpectrumCycle.speed_min  = GLORIOUS_SPEED_SLOW;
+    SpectrumCycle.speed      = GLORIOUS_SPEED_NORMAL;
+    SpectrumCycle.speed_max  = GLORIOUS_SPEED_FAST;
+    SpectrumCycle.color_mode = MODE_COLORS_NONE;
+    SpectrumCycle.value      = GLORIOUS_MODE_SPECTRUM_CYCLE;
+    modes.push_back(SpectrumCycle);
+
+    mode Flashing;
+    Flashing.name       = "Flashing";
+    Flashing.flags      = MODE_FLAG_HAS_SPEED;
+    Flashing.speed_min  = GLORIOUS_SPEED_SLOW;
+    Flashing.speed      = GLORIOUS_SPEED_NORMAL;
+    Flashing.speed_max  = GLORIOUS_SPEED_FAST;
+    Flashing.color_mode = MODE_COLORS_NONE;
+    Flashing.value      = GLORIOUS_MODE_FLASHING;
+    modes.push_back(Flashing);
+
+    mode Epilepsy;
+    Epilepsy.name       = "Epilepsy";
+    Epilepsy.flags      = 0;
+    Epilepsy.color_mode = MODE_COLORS_NONE;
+    Epilepsy.value      = GLORIOUS_MODE_EPILEPSY;
+    modes.push_back(Epilepsy);
+
+    mode RainbowSlow;
+    RainbowSlow.name       = "Random";
+    RainbowSlow.flags      = MODE_FLAG_HAS_SPEED;
+    RainbowSlow.speed_min  = GLORIOUS_SPEED_SLOW;
+    RainbowSlow.speed      = GLORIOUS_SPEED_NORMAL;
+    RainbowSlow.speed_max  = GLORIOUS_SPEED_FAST;
+    RainbowSlow.color_mode = MODE_COLORS_NONE;
+    RainbowSlow.value      = GLORIOUS_MODE_SLOW_RAINBOW;
+    modes.push_back(RainbowSlow);
+
+    mode Breathing;
+    Breathing.name       = "Breathing";
+    Breathing.flags      = MODE_FLAG_HAS_SPEED;
+    Breathing.speed_min  = GLORIOUS_MODE_BREATING_SLOW;
+    Breathing.speed      = GLORIOUS_MODE_BREATING_NORMAL;
+    Breathing.speed_max  = GLORIOUS_MODE_BREATING_FAST;
+    Breathing.color_mode = MODE_COLORS_NONE;
+    Breathing.value      = GLORIOUS_MODE_BREATHING;
+    modes.push_back(Breathing);
 
     SetupZones();
 }
@@ -81,12 +166,32 @@ void RGBController_GloriousModelO::UpdateSingleLED(int /*led*/)
 }
 void RGBController_GloriousModelO::SetCustomMode()
 {
-    active_mode = 0;
+    //active_mode = 2;
 }
 
 void RGBController_GloriousModelO::UpdateMode()
 {
-    gmo->SetMode(modes[active_mode].value);
+    unsigned int direction;
+
+    if (modes[active_mode].value == GLORIOUS_MODE_STATIC)
+    {
+
+    }
+    else
+    {
+        if (modes[active_mode].direction == MODE_DIRECTION_UP)
+        {
+            direction = GLORIOUS_DIRECTION_UP;
+        }
+        else
+        {
+            direction = GLORIOUS_DIRECTION_DOWN;
+        }
+
+        gmo->SetMode(modes[active_mode].value, modes[active_mode].speed, direction);
+    }
+
+
 }
 
 
