91414084d675ff3b962024e3eeaae311dfa678d3
Adam Honse <calcprogrammer1@gmail.com>
2020-06-18 10:50:25 -0500

    Move Off mode to second position in MSI Mystic Light mode list so that custom mode works

--- RGBController/RGBController_MSIMysticLight.cpp
+++ RGBController/RGBController_MSIMysticLight.cpp
@@ -138,8 +138,8 @@ void RGBController_MSIMysticLight::SetupModes()
     constexpr unsigned int RANDOM_ONLY  = MODE_FLAG_HAS_BRIGHTNESS | MODE_FLAG_HAS_SPEED | MODE_FLAG_HAS_RANDOM_COLOR;
     constexpr unsigned int COMMON       = RANDOM_ONLY | MODE_FLAG_HAS_PER_LED_COLOR;
 
-    SetupMode("Off", DISABLE, 0);
     SetupMode("Static", STATIC, MODE_FLAG_HAS_BRIGHTNESS | MODE_FLAG_HAS_PER_LED_COLOR);
+    SetupMode("Off", DISABLE, 0);
     SetupMode("Breathing", BREATHING, COMMON);
     SetupMode("Flashing", FLASHING, COMMON);
     SetupMode("Double flashing", DOUBLE_FLASHING, COMMON);
