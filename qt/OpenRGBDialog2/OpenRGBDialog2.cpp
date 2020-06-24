73051af2518e17e12d6f5619475ae84a4dabeb5a
Adam Honse <calcprogrammer1@gmail.com>
2020-06-23 23:04:12 -0500

    Select the main tab by default, not the fan tab

--- qt/OpenRGBDialog2/OpenRGBDialog2.cpp
+++ qt/OpenRGBDialog2/OpenRGBDialog2.cpp
@@ -1164,6 +1164,12 @@ void OpenRGBDialog2::ClearDevicesList()
             ui->InformationTabBar->removeTab(tab_idx);
         }
     }
+
+    for(int tab_idx = 0; tab_idx < ui->FanTabBar->count(); tab_idx++)
+    {
+        delete ui->FanTabBar->widget(tab_idx);
+    }
+    ui->FanTabBar->clear();
 }
 
 void OpenRGBDialog2::UpdateDevicesList()
