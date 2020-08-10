9cf32593ae751eaf2c23cb0369807e6df12bc954
xyz <a@b.c>
2020-08-10 15:57:15 +0200

    add PID of Logitech G502H

--- Controllers/LogitechController/LogitechControllerDetect.cpp
+++ Controllers/LogitechController/LogitechControllerDetect.cpp
@@ -34,6 +34,7 @@
 #define LOGITECH_G403_PID                       0xC083
 #define LOGITECH_G403H_PID                      0xC08F
 #define LOGITECH_G502_PS_PID                    0xC332
+#define LOGITECH_G502H_PID                      0xC08B
 #define LOGITECH_GPRO_WIRELESS_LIGHTSPEED_PID   0xC539
 #define LOGITECH_GPRO_WIRELESS_PID              0xC088
 
@@ -65,6 +66,7 @@ static const logitech_device device_list[] =
     { LOGITECH_VID,             LOGITECH_G403_PID,                      1,  DEVICE_TYPE_MOUSE,      "Logitech G403 Prodigy"                         },
     { LOGITECH_VID,             LOGITECH_G403H_PID,                     1,  DEVICE_TYPE_MOUSE,      "Logitech G403 Hero"                            },
     { LOGITECH_VID,             LOGITECH_G502_PS_PID,                   1,  DEVICE_TYPE_MOUSE,      "Logitech G502 Proteus Spectrum"                },
+    { LOGITECH_VID,             LOGITECH_G502H_PID,                     1,  DEVICE_TYPE_MOUSE,      "Logitech G502 Hero"                            },
     { LOGITECH_VID,             LOGITECH_GPRO_WIRELESS_LIGHTSPEED_PID,  2,  DEVICE_TYPE_MOUSE,      "Logitech G Pro Wireless Gaming Mouse"          },
     { LOGITECH_VID,             LOGITECH_GPRO_WIRELESS_PID,             2,  DEVICE_TYPE_MOUSE,      "Logitech G Pro Wireless Gaming Mouse (Wired)"  },
     /*---------------------------------------------------------------------------------------------------------------------------------------------*\
@@ -213,6 +215,7 @@ void DetectLogitechControllers(std::vector<RGBController*>& rgb_controllers)
                                 break;
 
                             case LOGITECH_G502_PS_PID:
+                            case LOGITECH_G502H_PID:
                                 {
                                     LogitechG502PSController* controller = new LogitechG502PSController(dev);
 
