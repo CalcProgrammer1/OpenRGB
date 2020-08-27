cd1ceed5df2862be057035bc591ef4ccc27e2c50
Ryakna <eduardo@queiroz.co>
2020-08-27 01:54:24 +0000

    Added detection for Logitech G502 Lightspeed when connected through the Logitech Powerplay Mousepad

--- Controllers/LogitechController/LogitechControllerDetect.cpp
+++ Controllers/LogitechController/LogitechControllerDetect.cpp
@@ -37,6 +37,7 @@
 #define LOGITECH_G502H_PID                      0xC08B
 #define LOGITECH_GPRO_WIRELESS_LIGHTSPEED_PID   0xC539
 #define LOGITECH_GPRO_WIRELESS_PID              0xC088
+#define LOGITECH_G502_LIGHTSPEED_POWERPLAY_PID  0xC53A
 
 typedef struct
 {
@@ -51,27 +52,28 @@ typedef struct
 
 static const logitech_device device_list[] =
 {
-    /*---------------------------------------------------------------------------------------------------------------------------------------------*\
-    | Keyboards                                                                                                                                     |
-    \*---------------------------------------------------------------------------------------------------------------------------------------------*/
-    { LOGITECH_VID,             LOGITECH_G810_1_PID,                    1,  DEVICE_TYPE_KEYBOARD,   "Logitech G810 Orion Spectrum"                  },
-    { LOGITECH_VID,             LOGITECH_G810_2_PID,                    1,  DEVICE_TYPE_KEYBOARD,   "Logitech G810 Orion Spectrum"                  },
-    { LOGITECH_VID,             LOGITECH_G512_PID,                      1,  DEVICE_TYPE_KEYBOARD,   "Logitech G512"                                 },
-    { LOGITECH_VID,             LOGITECH_G512_RGB_PID,                  1,  DEVICE_TYPE_KEYBOARD,   "Logitech G512 RGB"                             },
-    /*---------------------------------------------------------------------------------------------------------------------------------------------*\
-    | Mice                                                                                                                                          |
-    \*---------------------------------------------------------------------------------------------------------------------------------------------*/
-    { LOGITECH_VID,             LOGITECH_G203_PID,                      1,  DEVICE_TYPE_MOUSE,      "Logitech G203 Prodigy"                         },
-    { LOGITECH_VID,             LOGITECH_G203L_PID,                     1,  DEVICE_TYPE_MOUSE,      "Logitech G203 Lightsync"                       },
-    { LOGITECH_VID,             LOGITECH_G403_PID,                      1,  DEVICE_TYPE_MOUSE,      "Logitech G403 Prodigy"                         },
-    { LOGITECH_VID,             LOGITECH_G403H_PID,                     1,  DEVICE_TYPE_MOUSE,      "Logitech G403 Hero"                            },
-    { LOGITECH_VID,             LOGITECH_G502_PS_PID,                   1,  DEVICE_TYPE_MOUSE,      "Logitech G502 Proteus Spectrum"                },
-    { LOGITECH_VID,             LOGITECH_G502H_PID,                     1,  DEVICE_TYPE_MOUSE,      "Logitech G502 Hero"                            },
-    { LOGITECH_VID,             LOGITECH_GPRO_WIRELESS_LIGHTSPEED_PID,  2,  DEVICE_TYPE_MOUSE,      "Logitech G Pro Wireless Gaming Mouse"          },
-    { LOGITECH_VID,             LOGITECH_GPRO_WIRELESS_PID,             2,  DEVICE_TYPE_MOUSE,      "Logitech G Pro Wireless Gaming Mouse (Wired)"  },
-    /*---------------------------------------------------------------------------------------------------------------------------------------------*\
-    | Mousemats                                                                                                                                     |
-    \*---------------------------------------------------------------------------------------------------------------------------------------------*/
+    /*-------------------------------------------------------------------------------------------------------------------------------------------------*\
+    | Keyboards                                                                                                                                         |
+    \*-------------------------------------------------------------------------------------------------------------------------------------------------*/
+    { LOGITECH_VID,             LOGITECH_G810_1_PID,                    1,  DEVICE_TYPE_KEYBOARD,   "Logitech G810 Orion Spectrum"                      },
+    { LOGITECH_VID,             LOGITECH_G810_2_PID,                    1,  DEVICE_TYPE_KEYBOARD,   "Logitech G810 Orion Spectrum"                      },
+    { LOGITECH_VID,             LOGITECH_G512_PID,                      1,  DEVICE_TYPE_KEYBOARD,   "Logitech G512"                                     },
+    { LOGITECH_VID,             LOGITECH_G512_RGB_PID,                  1,  DEVICE_TYPE_KEYBOARD,   "Logitech G512 RGB"                                 },
+    /*-------------------------------------------------------------------------------------------------------------------------------------------------*\
+    | Mice                                                                                                                                              |
+    \*-------------------------------------------------------------------------------------------------------------------------------------------------*/
+    { LOGITECH_VID,             LOGITECH_G203_PID,                      1,  DEVICE_TYPE_MOUSE,      "Logitech G203 Prodigy"                             },
+    { LOGITECH_VID,             LOGITECH_G203L_PID,                     1,  DEVICE_TYPE_MOUSE,      "Logitech G203 Lightsync"                           },
+    { LOGITECH_VID,             LOGITECH_G403_PID,                      1,  DEVICE_TYPE_MOUSE,      "Logitech G403 Prodigy"                             },
+    { LOGITECH_VID,             LOGITECH_G403H_PID,                     1,  DEVICE_TYPE_MOUSE,      "Logitech G403 Hero"                                },
+    { LOGITECH_VID,             LOGITECH_G502_PS_PID,                   1,  DEVICE_TYPE_MOUSE,      "Logitech G502 Proteus Spectrum"                    },
+    { LOGITECH_VID,             LOGITECH_G502H_PID,                     1,  DEVICE_TYPE_MOUSE,      "Logitech G502 Hero"                                },
+    { LOGITECH_VID,             LOGITECH_GPRO_WIRELESS_LIGHTSPEED_PID,  2,  DEVICE_TYPE_MOUSE,      "Logitech G Pro Wireless Gaming Mouse"              },
+    { LOGITECH_VID,             LOGITECH_GPRO_WIRELESS_PID,             2,  DEVICE_TYPE_MOUSE,      "Logitech G Pro Wireless Gaming Mouse (Wired)"      },
+    { LOGITECH_VID,             LOGITECH_G502_LIGHTSPEED_POWERPLAY_PID, 2,  DEVICE_TYPE_MOUSE,      "Logitech G502 Lightspeed With Powerplay Mousepad"  },
+    /*-------------------------------------------------------------------------------------------------------------------------------------------------*\
+    | Mousemats                                                                                                                                         |
+    \*-------------------------------------------------------------------------------------------------------------------------------------------------*/
 };
 
 /******************************************************************************************\
@@ -228,6 +230,7 @@ void DetectLogitechControllers(std::vector<RGBController*>& rgb_controllers)
                                 
                             case LOGITECH_GPRO_WIRELESS_LIGHTSPEED_PID:
                             case LOGITECH_GPRO_WIRELESS_PID:
+                            case LOGITECH_G502_LIGHTSPEED_POWERPLAY_PID:
                                 {
                                     LogitechGProWirelessController* controller = new LogitechGProWirelessController(dev);
 
