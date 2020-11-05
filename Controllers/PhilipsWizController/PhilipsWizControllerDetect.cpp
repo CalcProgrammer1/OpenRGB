8eab2b624456c880cef85c06b8b34971472b090f
Adam Honse <calcprogrammer1@gmail.com>
2020-11-04 20:01:35 -0600

    Rework Philips Wiz detection to use Settings Manager

--- Controllers/PhilipsWizController/PhilipsWizControllerDetect.cpp
+++ Controllers/PhilipsWizController/PhilipsWizControllerDetect.cpp
@@ -2,20 +2,10 @@
 #include "PhilipsWizController.h"
 #include "RGBController.h"
 #include "RGBController_PhilipsWiz.h"
+#include "SettingsManager.h"
 #include <vector>
 #include <stdio.h>
 #include <stdlib.h>
-
-#include <fcntl.h>
-#include <string.h>
-#include <fstream>
-#include <iostream>
-#include <string>
-
-#ifndef WIN32
-#include <unistd.h>
-#include <dirent.h>
-#endif
 
 /******************************************************************************************\
 *                                                                                          *
@@ -30,23 +20,25 @@ void DetectPhilipsWizControllers(std::vector<RGBController*> &rgb_controllers)
     PhilipsWizController* new_controller;
     RGBController_PhilipsWiz* new_rgbcontroller;
 
-    std::ifstream infile;
-    char arg1[64];
+    json                    wiz_settings;
 
-    //Open settings file
-    infile.open("wiz.txt");
+    /*-------------------------------------------------*\
+    | Get Philips Wiz settings from settings manager    |
+    \*-------------------------------------------------*/
+    wiz_settings = ResourceManager::get()->GetSettingsManager()->GetSettings("Setting_PhilipsWizDevices");
 
-    if (infile.good())
+    /*-------------------------------------------------*\
+    | If the Wiz settings contains devices, process     |
+    \*-------------------------------------------------*/
+    if(wiz_settings.contains("devices"))
     {
-        for (std::string line; std::getline(infile, line); )
+        for(unsigned int device_idx = 0; device_idx < wiz_settings["devices"].size(); device_idx++)
         {
-            if (line == "")
+            if(wiz_settings["devices"][device_idx].contains("ip"))
             {
-                continue;
-            }
-            else
-            {
-                new_controller = new PhilipsWizController(line);
+                std::string wiz_ip = wiz_settings["devices"][device_idx]["ip"];
+
+                new_controller = new PhilipsWizController(wiz_ip);
                 new_rgbcontroller = new RGBController_PhilipsWiz(new_controller);
 
                 rgb_controllers.push_back(new_rgbcontroller);
