7628fe54910e19108873780d3642c2c581d09410
TheRogueZeta <6479694-TheRogueZeta@users.noreply.gitlab.com>
2020-12-30 17:00:14 -0800

    Add EVGA RTX 2080Ti XC Ultra

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -67,6 +67,7 @@
 #define EVGA_RTX2070_XC_OC_SUB_DEV              0x2173
 #define EVGA_RTX2070S_XC_ULTRA_SUB_DEV          0x3173
 #define EVGA_RTX2080_XC_GAMING_SUB_DEV          0x2182
+#define EVGA_RTX2080TI_XC_ULTRA_SUB_DEV         0x2383
 
 /*-----------------------------------------------------*\
 | GALAX / KFA2 Sub-Device IDs                           |
