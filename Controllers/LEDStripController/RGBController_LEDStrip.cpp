ae5143a520a649f28f07aa6aeaa6a1f555f9bfee
Adam Honse <calcprogrammer1@gmail.com>
2021-01-14 17:04:22 -0600

    Update description of LEDStrip since it now supports multiple protocols

--- Controllers/LEDStripController/RGBController_LEDStrip.cpp
+++ Controllers/LEDStripController/RGBController_LEDStrip.cpp
@@ -16,7 +16,7 @@ RGBController_LEDStrip::RGBController_LEDStrip(LEDStripController* ledstrip_ptr)
 
     name        = "LED Strip";
     type        = DEVICE_TYPE_LEDSTRIP;
-    description = "Keyboard Visualizer Arduino LED Strip Device";
+    description = "Serial LED Strip Device";
     location    = strip->GetLocation();
 
     mode Direct;
