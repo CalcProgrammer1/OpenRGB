1c5a11f4263953df9668d0597dfb5e52d6367341
TheRogueZeta <6479694-TheRogueZeta@users.noreply.gitlab.com>
2021-04-01 15:15:58 +0000

    Add Deskmini Addressable LED Strip PID + Detector

--- Controllers/ASRockPolychromeUSBController/ASRockPolychromeUSBControllerDetect.cpp
+++ Controllers/ASRockPolychromeUSBController/ASRockPolychromeUSBControllerDetect.cpp
@@ -9,15 +9,16 @@
 #include <stdlib.h>
 #include <hidapi/hidapi.h>
 
-/*-----------------------------------------------------*\
-| ASRock vendor ID                                      |
-\*-----------------------------------------------------*/
-#define ASROCK_VID                   0x26CE
+/*---------------------------------------------------------*\
+| ASRock vendor ID                                          |
+\*---------------------------------------------------------*/
+#define ASROCK_VID                                  0x26CE
 
-/*-----------------------------------------------------*\
-| ASRock product ID                                     |
-\*-----------------------------------------------------*/
-#define ASROCK_MOTHERBOARD_1_PID     0x01A2
+/*---------------------------------------------------------*\
+| ASRock product ID                                         |
+\*---------------------------------------------------------*/
+#define ASROCK_MOTHERBOARD_1_PID                    0x01A2
+#define ASROCK_DESKMINI_ADDRESSABLE_LED_STRIP_PID   0x01A6
 
 void DetectPolychromeUSBControllers(hid_device_info* info, const std::string& name)
 {
@@ -32,3 +33,4 @@ void DetectPolychromeUSBControllers(hid_device_info* info, const std::string& na
 }
 
 REGISTER_HID_DETECTOR("ASRock Polychrome USB", DetectPolychromeUSBControllers, ASROCK_VID, ASROCK_MOTHERBOARD_1_PID);
+REGISTER_HID_DETECTOR("ASRock Deskmini Addressable LED Strip", DetectPolychromeUSBControllers, ASROCK_VID, ASROCK_DESKMINI_ADDRESSABLE_LED_STRIP_PID);
