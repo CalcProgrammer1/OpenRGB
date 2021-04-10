1ba0104ddd6dc0e25922e738e9edd7f3b2e1bb72
Simbaclaws <hylke.hellinga@gmail.com>
2021-04-10 16:44:25 +0200

    Added ASUS RX 5700XT Strix Gaming OC and RX 580 Strix Gamnig OC
    Commits squashed by Adam Honse <calcprogrammer1@gmail.com>

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -79,6 +79,8 @@
 #define ASUS_GTX1060_STRIX                              0x85AC
 #define ASUS_ROG_GTX1660TI_OC                           0x86A5
 #define ASUS_VEGA64_STRIX                               0x04C4
+#define ASUS_RX5700XT_STRIX_GAMING_OC                   0x04E2
+#define ASUS_RX580_STRIX_GAMING_OC                      0x0517
 
 /*-----------------------------------------------------*\
 | EVGA Sub-Device IDs                                   |
