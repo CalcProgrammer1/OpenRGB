2d9e7f85aff1e87370df3a33bee2f7a478e1827d
Adam Honse <calcprogrammer1@gmail.com>
2021-04-13 19:33:30 -0500

    Add usage page filtering for MSI Mystic Light 162-byte controllers

--- Controllers/MSIMysticLightController/MSIMysticLightControllerDetect.cpp
+++ Controllers/MSIMysticLightController/MSIMysticLightControllerDetect.cpp
@@ -60,9 +60,9 @@ void DetectMSIMysticLight185Controllers(hid_device_info* info, const std::string
 /*-------------------------------------------------------------*\
 | MSI Mystic Light 162-byte Devices                             |
 \*-------------------------------------------------------------*/
-REGISTER_HID_DETECTOR("MSI Mystic Light MS_7B12", DetectMSIMysticLight162Controllers,   MSI_USB_VID,    0x7B12);
-REGISTER_HID_DETECTOR("MSI Mystic Light MS_7B17", DetectMSIMysticLight162Controllers,   MSI_USB_VID,    0x7B17);
-REGISTER_HID_DETECTOR("MSI Mystic Light MS_7B85", DetectMSIMysticLight162Controllers,   MSI_USB_VID,    0x7B85);
+REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7B12",    DetectMSIMysticLight162Controllers,   MSI_USB_VID,    0x7B12,   0x0001, 0x00);
+REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7B17",    DetectMSIMysticLight162Controllers,   MSI_USB_VID,    0x7B17,   0x0001, 0x00);
+REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7B85",    DetectMSIMysticLight162Controllers,   MSI_USB_VID,    0x7B85,   0x0001, 0x00);
 
 /*-------------------------------------------------------------*\
 | MSI Mystic Light 185-byte Devices                             |
