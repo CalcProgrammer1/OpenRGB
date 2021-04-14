cc9e9ad6e317be0d9016efa5b445b3ac3f5676ef
Adam Honse <calcprogrammer1@gmail.com>
2021-04-13 19:15:30 -0500

    Add ASUS ROG Strix GTX1080Ti Gaming

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -78,6 +78,7 @@
 \*-----------------------------------------------------*/
 #define ASUS_GTX1060_STRIX                              0x85AC
 #define ASUS_ROG_GTX1660TI_OC                           0x86A5
+#define ASUS_ROG_STRIX_GTX1080TI_GAMING                 0x85EA
 #define ASUS_VEGA64_STRIX                               0x04C4
 #define ASUS_RX5700XT_STRIX_GAMING_OC                   0x04E2
 #define ASUS_RX580_STRIX_GAMING_OC                      0x0517
