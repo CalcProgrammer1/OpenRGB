68b0287cdd1cac3a97067f21f4bf7357d04dc864
Adam Honse <calcprogrammer1@gmail.com>
2021-05-31 18:26:00 -0500

    Make detectors for QMK OpenRGB controller configurable

--- Controllers/QMKOpenRGBController/QMKOpenRGBControllerDetect.cpp
+++ Controllers/QMKOpenRGBController/QMKOpenRGBControllerDetect.cpp
@@ -7,6 +7,9 @@
 |  Jath03       28th May 2021                                         |
 \*-------------------------------------------------------------------*/
 
+#include <iostream>
+#include <string>
+
 #include "Detector.h"
 #include "QMKOpenRGBController.h"
 #include "RGBController.h"
@@ -23,26 +26,6 @@
 \*-----------------------------------------------------*/
 #define QMK_USAGE_PAGE                          0xFF60
 #define QMK_USAGE                               0x61
-
-/*-----------------------------------------------------*\
-| Massdrop                                              |
-\*-----------------------------------------------------*/
-#define MASSDROP_VID                            0x04D8
-#define MASSDROP_ALT_PID                        0xEED3
-#define MASSDROP_CTRL_PID                       0xEED2
-
-/*-----------------------------------------------------*\
-| SonixQMK                                              |
-\*-----------------------------------------------------*/
-#define SONIXQMK_VID                            0x0C45
-#define SONIXQMK_5004_PID                       0x5004
-#define SONIXQMK_5104_PID                       0x5104
-
-/*-----------------------------------------------------*\
-| KBDFans                                              |
-\*-----------------------------------------------------*/
-#define KBDFANS_VID                             0x4B42
-#define KBDFANS_K67_MKII_RGB_PID                0x1225
 
 void DetectQMKOpenRGBControllers(hid_device_info *info, const std::string&)
 {
@@ -67,11 +50,33 @@ void DetectQMKOpenRGBControllers(hid_device_info *info, const std::string&)
 
 void RegisterQMKDetectors()
 {
-    REGISTER_HID_DETECTOR_IPU("Massdrop Alt",           DetectQMKOpenRGBControllers, MASSDROP_VID, MASSDROP_ALT_PID,            1, QMK_USAGE_PAGE, QMK_USAGE);
-    REGISTER_HID_DETECTOR_IPU("Massdrop Ctrl",          DetectQMKOpenRGBControllers, MASSDROP_VID, MASSDROP_CTRL_PID,           1, QMK_USAGE_PAGE, QMK_USAGE);
-    REGISTER_HID_DETECTOR_IPU("SonixQMK 0C45:5004",     DetectQMKOpenRGBControllers, SONIXQMK_VID, SONIXQMK_5004_PID,           1, QMK_USAGE_PAGE, QMK_USAGE);
-    REGISTER_HID_DETECTOR_IPU("SonixQMK 0C45:5104",     DetectQMKOpenRGBControllers, SONIXQMK_VID, SONIXQMK_5104_PID,           1, QMK_USAGE_PAGE, QMK_USAGE);
-    REGISTER_HID_DETECTOR_IPU("KBDFans KBD67 MKII RGB", DetectQMKOpenRGBControllers, KBDFANS_VID,  KBDFANS_K67_MKII_RGB_PID,    1, QMK_USAGE_PAGE, QMK_USAGE);
+    /*-------------------------------------------------*\
+    | Get QMKOpenRGB settings                           |
+    \*-------------------------------------------------*/
+    json qmk_settings = ResourceManager::get()->GetSettingsManager()->GetSettings("QMKOpenRGBDevices");
+
+    if(qmk_settings.contains("devices"))
+    {
+        for(unsigned int device_idx = 0; device_idx < qmk_settings["devices"].size(); device_idx++)
+        {
+            if( qmk_settings["devices"][device_idx].contains("usb_pid")
+             && qmk_settings["devices"][device_idx].contains("usb_vid")
+             && qmk_settings["devices"][device_idx].contains("name"))
+            {
+                std::string usb_pid_str = qmk_settings["devices"][device_idx]["usb_pid"];
+                std::string usb_vid_str = qmk_settings["devices"][device_idx]["usb_vid"];
+                std::string name        = qmk_settings["devices"][device_idx]["name"];
+
+                /*-------------------------------------*\
+                | Parse hex string to integer           |
+                \*-------------------------------------*/
+                unsigned short usb_pid  = std::stoi(usb_pid_str, 0, 16);
+                unsigned short usb_vid  = std::stoi(usb_vid_str, 0, 16);
+
+                REGISTER_DYNAMIC_HID_DETECTOR_IPU(name, DetectQMKOpenRGBControllers, usb_vid, usb_pid, 1, QMK_USAGE_PAGE, QMK_USAGE);
+            }
+        }
+    }
 }
 
 REGISTER_DYNAMIC_DETECTOR("QMK OpenRGB Devices", RegisterQMKDetectors);
