e52ba498151cb721f17ce71e29bb0a4f983107d4
Cheerpipe <cheerpipe@gmail.com>
2021-07-06 14:04:14 -0400

    Add Comet Lake H SMBUS PCI ID
    From a 10900k with a z490 chipset.
    
    More info on: https://github.com/torvalds/linux/blob/master/drivers/i2c/busses/i2c-i801.c

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -31,6 +31,7 @@
 #define INTEL_ICH10_SMBUS_DEV                           0x3A30
 #define INTEL_COFFEE_LAKE_SMBUS_DEV                     0xA2A3
 #define INTEL_COFFEE_LAKE_S_SMBUS_DEV                   0xA323
+#define INTEL_COMET_LAKE_H_SMBUS_DEV                    0x06A3
 
 /*-----------------------------------------------------*\
 | nVidia Device IDs                                     |
@@ -217,11 +218,13 @@
     if(((ven == AMD_VEN) && (dev == AMD_FCH_SMBUS_DEV)) ||  \
     ((ven == INTEL_VEN) && (dev == INTEL_ICH10_SMBUS_DEV)) || \
     ((ven == INTEL_VEN) && (dev == INTEL_COFFEE_LAKE_SMBUS_DEV)) || \
-    ((ven == INTEL_VEN) && (dev == INTEL_COFFEE_LAKE_S_SMBUS_DEV)))
+    ((ven == INTEL_VEN) && (dev == INTEL_COFFEE_LAKE_S_SMBUS_DEV)) || \
+    ((ven == INTEL_VEN) && (dev == INTEL_COMET_LAKE_H_SMBUS_DEV)))
 
 #define IF_MOBO_SMBUS(ven, dev)                             \
     if((ven == 0) ||                                        \
     ((ven == AMD_VEN) && (dev == AMD_FCH_SMBUS_DEV)) ||     \
     ((ven == INTEL_VEN) && (dev == INTEL_ICH10_SMBUS_DEV)) || \
     ((ven == INTEL_VEN) && (dev == INTEL_COFFEE_LAKE_SMBUS_DEV)) || \
-    ((ven == INTEL_VEN) && (dev == INTEL_COFFEE_LAKE_S_SMBUS_DEV)))
+    ((ven == INTEL_VEN) && (dev == INTEL_COFFEE_LAKE_S_SMBUS_DEV)) || \
+    ((ven == INTEL_VEN) && (dev == INTEL_COMET_LAKE_H_SMBUS_DEV)))
