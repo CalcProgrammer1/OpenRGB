1ba3c6addeab9e1b6d6693518c67a27afa9a2889
TheRogueZeta <6479694-TheRogueZeta@users.noreply.gitlab.com>
2021-08-06 13:15:16 -0700

    Add RTX 3060 v2 Device ID & update Gigabyte detector for #1676

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -60,6 +60,7 @@
 #define NVIDIA_RTX2080S_DEV                             0x1E81
 #define NVIDIA_RTX2080TI_DEV                            0x1E07
 #define NVIDIA_RTX3060_DEV                              0x2503
+#define NVIDIA_RTX3060_V2_DEV                           0x2504
 #define NVIDIA_RTX3060TI_DEV                            0x2486
 #define NVIDIA_RTX3070_DEV                              0x2484
 #define NVIDIA_RTX3070TI_DEV                            0x2482
