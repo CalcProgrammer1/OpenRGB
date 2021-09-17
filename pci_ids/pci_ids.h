822a12ac29291c383217087120ae7539596c471d
TheRogueZeta <6479694-TheRogueZeta@users.noreply.gitlab.com>
2021-09-17 11:42:27 -0700

    Add EVGA RTX 2070 SUPER Black device ID

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -129,6 +129,7 @@
 #define EVGA_GTX1080_FTW_SUB_DEV                        0x6286
 #define EVGA_RTX2070_XC_GAMING_SUB_DEV                  0x2172
 #define EVGA_RTX2070_XC_OC_SUB_DEV                      0x2173
+#define EVGA_RTX2070S_BLACK_SUB_DEV                     0x3071
 #define EVGA_RTX2070S_XC_ULTRA_SUB_DEV                  0x3173
 #define EVGA_RTX2070S_XC_ULTRA_PLUS_SUB_DEV             0x3175
 #define EVGA_RTX2080_XC_BLACK_SUB_DEV                   0x2082
