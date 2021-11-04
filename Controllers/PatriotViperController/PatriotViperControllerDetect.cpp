4738dfbe8d293ebed77cb5e4275493147d5b94b2
Qwex <ritt.alex@gmail.com>
2021-11-04 04:17:44 +0000

    Fix Patriot Viper ram detection

--- Controllers/PatriotViperController/PatriotViperControllerDetect.cpp
+++ Controllers/PatriotViperController/PatriotViperControllerDetect.cpp
@@ -1,5 +1,6 @@
 #include "Detector.h"
 #include "PatriotViperController.h"
+#include "LogManager.h"
 #include "RGBController.h"
 #include "RGBController_PatriotViper.h"
 #include "i2c_smbus.h"
@@ -9,6 +10,7 @@
 #include <stdlib.h>
 
 using namespace std::chrono_literals;
+#define PATRIOT_CONTROLLER_NAME "Patriot Viper"
 
 /******************************************************************************************\
 *                                                                                          *
@@ -32,49 +34,50 @@ void DetectPatriotViperControllers(std::vector<i2c_smbus_interface*> &busses)
 
         IF_DRAM_SMBUS(busses[bus]->pci_vendor, busses[bus]->pci_device)
         {
+            int res = busses[bus]->i2c_smbus_read_byte_data(0x36, 0x00);
+
+            if(res < 0)
+            {
+                continue;
+            }
+            std::this_thread::sleep_for(1ms);
+
             for(int slot_addr = 0x50; slot_addr <= 0x57; slot_addr++)
             {
                 // Test for Patriot Viper RGB SPD at slot_addr
                 // This test was copied from Viper RGB software
                 // Tests SPD addresses in order: 0x00, 0x40, 0x41, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68
-                bool pass = false;
 
-                int res = busses[bus]->i2c_smbus_read_byte(slot_addr);
+                busses[bus]->i2c_smbus_write_byte_data(0x36, 0x00, 0xFF);
 
-                if (res >= 0)
+                std::this_thread::sleep_for(1ms);
+
+                int res = busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x00);
+
+                LOG_DEBUG("[%s] Trying to read 0x%02X RAM module data at 0x00 expect: 0x23 res: %02X", PATRIOT_CONTROLLER_NAME, slot_addr, res);
+                if(res == 0x23)
                 {
-                    pass = true;
-                }
-
-                if(pass)
-                {
-                    busses[bus]->i2c_smbus_write_byte_data(0x36, 0x00, 0xFF);
+                    busses[bus]->i2c_smbus_write_byte_data(0x37, 0x00, 0xFF);
 
                     std::this_thread::sleep_for(1ms);
 
-                    if(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x00) == 0x23)
+                    if((busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x40) == 0x85)
+                    &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x41) == 0x02)
+                    &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x61) == 0x4D)
+                    &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x62) == 0x49)
+                    &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x63) == 0x43)
+                    &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x64) == 0x53)
+                    &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x65) == 0x59)
+                    &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x66) == 0x53)
+                    &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x67) == 0x5f)
+                    &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x68) == 0x44))
                     {
-                        busses[bus]->i2c_smbus_write_byte_data(0x37, 0x00, 0xFF);
-
-                        std::this_thread::sleep_for(1ms);
-
-                        if((busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x40) == 0x85)
-                        &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x41) == 0x02)
-                        &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x61) == 0x4D)
-                        &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x62) == 0x49)
-                        &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x63) == 0x43)
-                        &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x64) == 0x53)
-                        &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x65) == 0x59)
-                        &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x66) == 0x53)
-                        &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x67) == 0x5f)
-                        &&(busses[bus]->i2c_smbus_read_byte_data(slot_addr, 0x68) == 0x44))
-                        {
-                            slots_valid |= (1 << (slot_addr - 0x50));
-                        }
+                        LOG_DEBUG("[%s] The 0x%02X RAM module detected", PATRIOT_CONTROLLER_NAME, slot_addr);
+                        slots_valid |= (1 << (slot_addr - 0x50));
                     }
-
-                    std::this_thread::sleep_for(1ms);
                 }
+
+                std::this_thread::sleep_for(1ms);
             }
 
             if(slots_valid != 0)
@@ -88,4 +91,4 @@ void DetectPatriotViperControllers(std::vector<i2c_smbus_interface*> &busses)
 
 }   /* DetectPatriotViperControllers() */
 
-REGISTER_I2C_DETECTOR("Patriot Viper", DetectPatriotViperControllers);
+REGISTER_I2C_DETECTOR(PATRIOT_CONTROLLER_NAME, DetectPatriotViperControllers);
