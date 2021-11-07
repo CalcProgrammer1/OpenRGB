c3e06d732ed58eefc76a08440ceb69eeea22f311
Adam Honse <calcprogrammer1@gmail.com>
2021-11-07 15:31:45 -0600

    Attempt to work around Windows compile issue

--- ResourceManager.cpp
+++ ResourceManager.cpp
@@ -1110,17 +1110,19 @@ void ResourceManager::DetectDevicesThreadFunction()
     \*-------------------------------------------------*/
     if(i2c_interface_fail)
     {
-        LOG_DIALOG("One or more I2C/SMBus interfaces failed to initialize.\r"
+        const char* i2c_message =   "One or more I2C/SMBus interfaces failed to initialize.\r"
 #ifdef _WIN32
-                   "On Windows, this is usually caused by a failure to load the inpout32 driver.\r"
-                   "You must run OpenRGB as administrator at least once to allow inpout32 to set up.\r"
+                                    "On Windows, this is usually caused by a failure to load the inpout32 driver.\r"
+                                    "You must run OpenRGB as administrator at least once to allow inpout32 to set up.\r"
 #endif
 #ifdef __linux__
-                   "On Linux, this is usually because the i2c-dev module is not loaded.\r"
-                   "You must load the i2c-dev module along with the correct i2c driver for your motherboard.\r"
-                   "This is usually i2c-piix4 for AMD systems and i2c-i801 for Intel systems.\r"
+                                    "On Linux, this is usually because the i2c-dev module is not loaded.\r"
+                                    "You must load the i2c-dev module along with the correct i2c driver for your motherboard.\r"
+                                    "This is usually i2c-piix4 for AMD systems and i2c-i801 for Intel systems.\r"
 #endif
-                   "See https://help.openrgb.org for additional troubleshooting.");
+                                    "See https://help.openrgb.org for additional troubleshooting.";
+
+        LOG_DIALOG("%s", i2c_message);
     }
 }
 
