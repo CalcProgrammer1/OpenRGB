695ff3c8a0dd094749a30b9f19fd42c6e3f62ee1
TheRogueZeta <6479694-TheRogueZeta@users.noreply.gitlab.com>
2021-11-06 21:41:19 -0700

    Add MSI MPG Z590 GAMING EDGE WIFI PID

--- Controllers/MSIMysticLightController/MSIMysticLightControllerDetect.cpp
+++ Controllers/MSIMysticLightController/MSIMysticLightControllerDetect.cpp
@@ -91,6 +91,7 @@ REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7C92",    DetectMSIMysticLight185C
 REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7C94",    DetectMSIMysticLight185Controllers,   MSI_USB_VID,    0x7C94,   0x0001, 0x00);
 REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7C95",    DetectMSIMysticLight185Controllers,   MSI_USB_VID,    0x7C95,   0x0001, 0x00);
 REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7D06",    DetectMSIMysticLight185Controllers,   MSI_USB_VID,    0x7D06,   0x0001, 0x00);
+REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7D07",    DetectMSIMysticLight185Controllers,   MSI_USB_VID,    0x7D07,   0x0001, 0x00); // MPG Z590 GAMING EDGE WIFI
 REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7D09",    DetectMSIMysticLight185Controllers,   MSI_USB_VID,    0x7D09,   0x0001, 0x00);
 REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7D13",    DetectMSIMysticLight185Controllers,   MSI_USB_VID,    0x7D13,   0x0001, 0x00);
 REGISTER_HID_DETECTOR_PU("MSI Mystic Light MS_7D15",    DetectMSIMysticLight185Controllers,   MSI_USB_VID,    0x7D15,   0x0001, 0x00);
