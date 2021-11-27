af9af2fbe3491f2d90fd20004468375cc8c4a419
Triqqo <triqqo@gmail.com>
2021-11-27 18:31:18 +0000

    Added ASUS TUF 3090 (non-OC) detection

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -147,6 +147,7 @@
 #define ASUS_ROG_STRIX_RTX_3080TI_O12G_GAMING           0x8807
 #define ASUS_ROG_STRIX_RTX_3090_O24G_GAMING             0x87AF
 #define ASUS_ROG_STRIX_RTX_3090_O24G_GAMING_WHITE       0x87D9
+#define ASUS_TUF_RTX_3090_O24G                          0x87B5
 #define ASUS_TUF_RTX_3090_O24G_OC                       0x87B3
 
 /*-----------------------------------------------------*\
