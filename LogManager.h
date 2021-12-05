6b1e217afac9272333b63e6be70be849b07bed31
TheRogueZeta <6479694-TheRogueZeta@users.noreply.gitlab.com>
2021-12-05 06:08:37 +0000

    Fix GPU print messages to use capital letters

--- LogManager.h
+++ LogManager.h
@@ -15,7 +15,7 @@
 #define SMBUS_CHECK_DEVICE_MESSAGE_EN   "[%s] Bus %02d is a motherboard and the subvendor matches the one for %s, looking for a device at %02X"
 #define SMBUS_CHECK_DEVICE_FAILURE_EN   "[%s] Bus %02d is not a motherboard or the subvendor does not match the one for %s, skipping detection"
 
-#define GPU_DETECT_MESSAGE              "[%s] Found a device match at Bus %02d for Device %04x and SubDevice %04x: %s"
+#define GPU_DETECT_MESSAGE              "[%s] Found a device match at Bus %02d for Device %04X and SubDevice %04X: %s"
 
 using json = nlohmann::json;
 
