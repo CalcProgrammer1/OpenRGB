f89c56d8649f7f135169d7ff5efe9a7611cc5586
morg <morgan.guimard@gmail.com>
2022-01-04 00:11:08 +0100

    Review TabLabel UI. Fix #2055

--- qt/TabLabel.cpp
+++ qt/TabLabel.cpp
@@ -8,14 +8,6 @@ Ui::TabLabel::TabLabel(QString icon, QString name) :
     ui->setupUi(this);
     ui->icon->setText("<img src=':/" + icon + "' height='16' width='16'  />");
     ui->name->setText(name);
-
-    /*---------------------------------------------------------*\
-    | Make sure to properly set the tab label height depending  |
-    | on the font in use.                                       |
-    \*---------------------------------------------------------*/
-    QFontMetrics fontMetrics(font());
-    QRect rect = fontMetrics.boundingRect(0, 0, width(), height(), Qt::TextWordWrap | Qt::TextExpandTabs, name);
-    setFixedHeight(std::max(rect.height(), ui->icon->height()));
 }
 
 Ui::TabLabel::~TabLabel()
