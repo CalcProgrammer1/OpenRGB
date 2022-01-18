97e154ea84c46651d71553735aa7dc1f5cf8f31f
1vivy <1vivy@tutanota.com>
2022-01-18 02:52:22 +0000

    Add ASUS KO RTX 3060Ti O8G V2 GAMING GPU

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -148,6 +148,7 @@
 #define ASUS_KO_RTX_3060_OC_O12G_GAMING                 0x87FB
 #define ASUS_ROG_STRIX_RTX_3060_O12G_GAMING             0x87F3
 #define ASUS_ROG_STRIX_RTX_3060_O12G_LHR_GAMING         0x8818
+#define ASUS_KO_RTX3060TI_O8G_V2_GAMING                 0x883E
 #define ASUS_TUF_RTX_3060_TI_O8G_OC                     0x87C6
 #define ASUS_TUF_RTX_3060_TI_O8G_OC_V2                  0x8827
 #define ASUS_ROG_STRIX_RTX_3070_OC                      0x87B8
