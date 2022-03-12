a07425026c1d2c7200b8b353bc54603108a65f75
Chris <galdarian@gmail.com>
2022-03-12 12:01:52 +1100

    Initial commit for the ASUS TUF RTX3070TI O8G V2 Gaming GPU to resolve #2300
    + Add id to pci_ids.h
    + Register detector in ENESMBusControllerDetect.cpp
    + Link to [webpage](https://www.asus.com/Motherboards-Components/Graphics-Cards/TUF-Gaming/TUF-RTX3070TI-O8G-GAMING/)

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -168,6 +168,7 @@
 #define ASUS_TUF_RTX_3070_O8G_GAMING                    0x87C1
 #define ASUS_TUF_RTX_3070_O8G_V2_GAMING                 0x8825
 #define ASUS_ROG_STRIX_RTX_3070TI_O8G_GAMING            0x880E
+#define ASUS_TUF_RTX_3070TI_O8G_V2_GAMING               0x8812
 #define ASUS_TUF_RTX_3070TI_O8G_GAMING                  0x8813
 #define ASUS_ROG_STRIX_RTX_3080_O10G_GAMING             0x87AC
 #define ASUS_ROG_STRIX_RTX_3080_O10G_WHITE              0x87D1
