a182f7a96f0f373fbb035fa6d12b39c12969a547
TheRogueZeta <6479694-TheRogueZeta@users.noreply.gitlab.com>
2022-06-30 21:05:58 +0000

    Initial Commit for ASUS TUF RTX 3080 10G GAMING with SVID 0x87B2

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -193,6 +193,7 @@
 #define ASUS_ROG_STRIX_RTX_3080_O10G_WHITE              0x87D1
 #define ASUS_ROG_STRIX_RTX_3080_O10G_V2_WHITE           0x8830
 #define ASUS_TUF_RTX_3080_10G_GAMING_PD                 0x87C4
+#define ASUS_TUF_RTX_3080_10G_GAMING                    0x87B2
 #define ASUS_TUF_RTX_3080_O10G_OC                       0x87B0
 #define ASUS_TUF_RTX_3080_O10G_V2_GAMING_8822           0x8822
 #define ASUS_TUF_RTX_3080_O10G_V2_GAMING                0x8823
