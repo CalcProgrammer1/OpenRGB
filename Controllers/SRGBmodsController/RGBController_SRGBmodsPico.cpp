16effbe35befc3d480e27bec64781b83b8080039
Adam Honse <calcprogrammer1@gmail.com>
2022-07-22 03:23:31 +0000

    Initial support for SRGBmods Raspberry Pi Pico LED Controller

---
+++ Controllers/SRGBmodsController/RGBController_SRGBmodsPico.cpp
@@ -0,0 +1,157 @@
+/*-----------------------------------------*\
+|  RGBController_SRGBmodsPico.cpp           |
+|                                           |
+|  Generic RGB Interface for SRGBmods       |
+|  Raspberry Pi Pico LED Controller         |
+|                                           |
+|  Adam Honse (CalcProgrammer1) 7/21/2022   |
+\*-----------------------------------------*/
+
+#include "RGBController_SRGBmodsPico.h"
+
+/**------------------------------------------------------------------*\
+    @name SRGBmods Raspberry Pi Pico LED Controller
+    @category LEDStrip
+    @type USB
+    @save :x:
+    @direct :white_check_mark:
+    @effects :white_check_mark:
+    @detectors DetectSRGBmodsControllers
+    @comment
+\*-------------------------------------------------------------------*/
+
+RGBController_SRGBmodsPico::RGBController_SRGBmodsPico(SRGBmodsPicoController* controller_ptr)
+{
+    controller  = controller_ptr;
+
+    name        = "SRGBmods Device";
+    vendor      = "SRGBmods.net";
+    description = "SRGBmods Pico LED Controller Device";
+    type        = DEVICE_TYPE_LEDSTRIP;
+    location    = controller->GetLocationString();
+    serial      = controller->GetSerialString();
+
+    mode Direct;
+    Direct.name       = "Direct";
+    Direct.value      = 0xFFFF;
+    Direct.flags      = MODE_FLAG_HAS_PER_LED_COLOR;
+    Direct.color_mode = MODE_COLORS_PER_LED;
+    modes.push_back(Direct);
+
+    SetupZones();
+}
+
+RGBController_SRGBmodsPico::~RGBController_SRGBmodsPico()
+{
+    delete controller;
+}
+
+void RGBController_SRGBmodsPico::SetupZones()
+{
+    /*-------------------------------------------------*\
+    | Only set LED count on the first run               |
+    \*-------------------------------------------------*/
+    bool first_run = false;
+
+    if(zones.size() == 0)
+    {
+        first_run = true;
+    }
+
+    /*-------------------------------------------------*\
+    | Clear any existing color/LED configuration        |
+    \*-------------------------------------------------*/
+    leds.clear();
+    colors.clear();
+    zones.resize(SRGBMODS_PICO_NUM_CHANNELS);
+
+    /*-------------------------------------------------*\
+    | Set zones and leds                                |
+    \*-------------------------------------------------*/
+    for(unsigned int channel_idx = 0; channel_idx < SRGBMODS_PICO_NUM_CHANNELS; channel_idx++)
+    {
+        char ch_idx_string[2];
+        sprintf(ch_idx_string, "%d", channel_idx + 1);
+
+        zones[channel_idx].name     = "Channel ";
+        zones[channel_idx].name.append(ch_idx_string);
+        zones[channel_idx].type     = ZONE_TYPE_LINEAR;
+
+        /*-------------------------------------------------*\
+        | The maximum number of LEDs per channel is 512     |
+        | according to https://srgbmods.net/picoled/        |
+        \*-------------------------------------------------*/
+        zones[channel_idx].leds_min   = 0;
+        zones[channel_idx].leds_max   = 512;
+
+        if(first_run)
+        {
+            zones[channel_idx].leds_count = 0;
+        }
+
+        zones[channel_idx].matrix_map = NULL;
+
+        for(unsigned int led_ch_idx = 0; led_ch_idx < zones[channel_idx].leds_count; led_ch_idx++)
+        {
+            char led_idx_string[4];
+            sprintf(led_idx_string, "%d", led_ch_idx + 1);
+
+            led new_led;
+            new_led.name = "LED ";
+            new_led.name.append(led_idx_string);
+
+            leds.push_back(new_led);
+            leds_channel.push_back(channel_idx);
+        }
+    }
+
+    SetupColors();
+}
+
+void RGBController_SRGBmodsPico::ResizeZone(int zone, int new_size)
+{
+    if((size_t) zone >= zones.size())
+    {
+        return;
+    }
+
+    if(((unsigned int)new_size >= zones[zone].leds_min) && ((unsigned int)new_size <= zones[zone].leds_max))
+    {
+        zones[zone].leds_count = new_size;
+
+        SetupZones();
+    }
+}
+
+void RGBController_SRGBmodsPico::DeviceUpdateLEDs()
+{
+    for(std::size_t zone_idx = 0; zone_idx < zones.size(); zone_idx++)
+    {
+        if(zones[zone_idx].leds_count > 0)
+        {
+            controller->SetChannelLEDs(zone_idx, zones[zone_idx].colors, zones[zone_idx].leds_count);
+        }
+    }
+}
+
+void RGBController_SRGBmodsPico::UpdateZoneLEDs(int zone)
+{
+    controller->SetChannelLEDs(zone, zones[zone].colors, zones[zone].leds_count);
+}
+
+void RGBController_SRGBmodsPico::UpdateSingleLED(int led)
+{
+    unsigned int channel = leds_channel[led];
+
+    controller->SetChannelLEDs(channel, zones[channel].colors, zones[channel].leds_count);
+}
+
+void RGBController_SRGBmodsPico::SetCustomMode()
+{
+    active_mode = 0;
+}
+
+void RGBController_SRGBmodsPico::DeviceUpdateMode()
+{
+    DeviceUpdateLEDs();
+}
---
+++ Controllers/SRGBmodsController/SRGBmodsPicoController.cpp
@@ -0,0 +1,129 @@
+/*-----------------------------------------*\
+|  ZalmanZSyncController.cpp                |
+|                                           |
+|  Driver for SRGBmods Raspberry Pi Pico    |
+|  LED Controller                           |
+|                                           |
+|  Adam Honse (CalcProgrammer1) 7/21/2022   |
+\*-----------------------------------------*/
+
+#include "SRGBmodsPicoController.h"
+#include <cstring>
+
+using namespace std::chrono_literals;
+
+SRGBmodsPicoController::SRGBmodsPicoController(hid_device* dev_handle, const char* path)
+{
+    dev         = dev_handle;
+    location    = path;
+
+    /*-----------------------------------------------------*\
+    | The SRGBmods Pico controller requires a packet within |
+    | 10 seconds of sending the lighting change in order    |
+    | to not revert back into hardware mode.  Start a thread|
+    | to continuously send a keepalive packet every 5s      |
+    \*-----------------------------------------------------*/
+    keepalive_thread_run = 1;
+    keepalive_thread     = new std::thread(&SRGBmodsPicoController::KeepaliveThread, this);
+}
+
+SRGBmodsPicoController::~SRGBmodsPicoController()
+{
+    keepalive_thread_run = 0;
+    keepalive_thread->join();
+    delete keepalive_thread;
+
+    hid_close(dev);
+}
+
+void SRGBmodsPicoController::KeepaliveThread()
+{
+    while(keepalive_thread_run.load())
+    {
+        if((std::chrono::steady_clock::now() - last_commit_time) > std::chrono::seconds(1))
+        {
+            SendPacket(1, 0, 0, false, NULL, 0);
+        }
+        std::this_thread::sleep_for(5s);
+    }
+}
+
+std::string SRGBmodsPicoController::GetLocationString()
+{
+    return("HID: " + location);
+}
+
+std::string SRGBmodsPicoController::GetSerialString()
+{
+    wchar_t serial_string[128];
+    int ret = hid_get_serial_number_string(dev, serial_string, 128);
+
+    if(ret != 0)
+    {
+        return("");
+    }
+
+    std::wstring return_wstring = serial_string;
+    std::string return_string(return_wstring.begin(), return_wstring.end());
+
+    return(return_string);
+}
+
+void SRGBmodsPicoController::SetChannelLEDs(unsigned char channel, RGBColor* colors, unsigned int num_colors)
+{
+    unsigned int num_packets    = num_colors / 20 + ((num_colors % 20) > 0);
+    unsigned int color_idx      = 0;
+
+    for(unsigned int packet_idx = 0; packet_idx < num_packets; packet_idx++)
+    {
+        unsigned int colors_in_packet = 20;
+
+        if(num_colors - color_idx < colors_in_packet)
+        {
+            colors_in_packet = num_colors - color_idx;
+        }
+
+        SendPacket(channel, packet_idx + 1, num_packets, false, &colors[color_idx * 3], colors_in_packet);
+
+        color_idx += colors_in_packet;
+    }
+}
+
+void SRGBmodsPicoController::SendPacket
+    (
+    unsigned char   channel,
+    unsigned char   this_packet_id,
+    unsigned char   last_packet_id,
+    bool            reset,
+    RGBColor*       colors,
+    unsigned int    num_colors
+    )
+{
+    unsigned char   usb_buf[65];
+
+    /*-----------------------------------------------------*\
+    | Zero out buffer                                       |
+    \*-----------------------------------------------------*/
+    memset(usb_buf, 0x00, sizeof(usb_buf));
+
+    /*-----------------------------------------------------*\
+    | Set up Firmware Version Request packet                |
+    \*-----------------------------------------------------*/
+    usb_buf[0x00]   = 0x00;             /* hidapi Report ID*/
+    usb_buf[0x01]   = this_packet_id;   /* This Packet ID  */
+    usb_buf[0x02]   = reset;            /* Reset Flag      */
+    usb_buf[0x03]   = last_packet_id;   /* Last Packet ID  */
+    usb_buf[0x04]   = channel + 1;      /* Channel (1 or 2)*/
+
+    for(unsigned int color_idx = 0; color_idx < num_colors; color_idx++)
+    {
+        usb_buf[0x05 + (color_idx * 3)] = RGBGetRValue(colors[color_idx]);
+        usb_buf[0x06 + (color_idx * 3)] = RGBGetGValue(colors[color_idx]);
+        usb_buf[0x07 + (color_idx * 3)] = RGBGetBValue(colors[color_idx]);
+    }
+
+    /*-----------------------------------------------------*\
+    | Send packet                                           |
+    \*-----------------------------------------------------*/
+    hid_write(dev, usb_buf, 65);
+}
