ecdad8c3de5f0def86617ee2890e144fa2c0d30a
MOVZX <movzx@yahoo.com>
2022-08-06 19:40:43 +0000

    Add support for MSI Radeon RX 6700 XT Gaming X & MSI Radeon RX 6800 XT Gaming X Trio

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -432,6 +432,9 @@
 #define MSI_RTX3090_SUPRIM_X_SUB_DEV                    0x3882
 #define MSI_RTX3090TI_SUPRIM_X_SUB_DEV                  0x5090
 #define MSI_RX6600XT_GAMING_X_SUB_DEV                   0x5020
+#define MSI_RX6700XT_GAMING_X_SUB_DEV                   0x3981
+#define MSI_RX6800_GAMING_X_TRIO_SUB_DEV                0x3961
+#define MSI_RX6800XT_GAMING_X_TRIO_SUB_DEV              0x3951
 #define MSI_RX6800XT_GAMING_Z_TRIO_SUB_DEV              0x3953
 
 /*-----------------------------------------------------*\
