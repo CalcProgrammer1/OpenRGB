CalcProgrammer1/OpenRGB

commit 942a842a321437aad2d4c2080c18dfab72ca482b

Hiding non-keys from Razer keymaps
+ Setting matrix map key value if not found in defined layout
+ Correcting layout for `blackwidow_chroma_te_keymap` to include missing keys

Controllers/RazerController/RazerDevices.h
@@ -432,7 +432,9 @@ static const razer_key blackwidow_chroma_te_keymap[] =
     {   0,      5,      1,              KEY_EN_LEFT_CONTROL                 },
     {   0,      5,      2,              KEY_EN_LEFT_WINDOWS                 },
     {   0,      5,      3,              KEY_EN_LEFT_ALT                     },
+    {   0,      5,      7,              KEY_EN_SPACE                        },
     {   0,      5,      11,             KEY_EN_RIGHT_ALT                    },
+    {   0,      5,      12,             KEY_EN_RIGHT_FUNCTION               },
     {   0,      5,      13,             KEY_EN_MENU                         },
     {   0,      5,      14,             KEY_EN_RIGHT_CONTROL                },
     {   0,      5,      15,             KEY_EN_LEFT_ARROW                   },
@@ -3172,8 +3174,8 @@ static const razer_device blackwidow_x_chroma_te_device =
         NULL,
         NULL
     },
-    NULL,
-    0
+    blackwidow_chroma_te_keymap,
+    BLACKWIDOW_CHROMA_TE_KEYMAP_SIZE
 };
 
 /*-------------------------------------------------------------*\

Controllers/RazerController/RGBController_Razer.cpp
@@ -183,6 +183,8 @@ void RGBController_Razer::SetupZones()
 
                 if(device_list[device_index]->keymap != NULL)
                 {
+                    bool not_found = true;
+
                     for(unsigned int i = 0; i < device_list[device_index]->keymap_size; i++)
                     {
                         if(zone_id == device_list[device_index]->keymap[i].zone &&
@@ -190,7 +192,14 @@ void RGBController_Razer::SetupZones()
                            col_id  == device_list[device_index]->keymap[i].col)
                         {
                             new_led->name = device_list[device_index]->keymap[i].name;
+                            not_found = false;
+                            break;
                         }
+                    }
+
+                    if(not_found)
+                    {
+                        zones[zone_id].matrix_map->map[row_id * zones[zone_id].matrix_map->width + col_id] = NA;
                     }
                 }
 

Controllers/RazerController/RGBController_Razer.h
@@ -10,6 +10,8 @@
 #include "RGBController.h"
 #include "RazerController.h"
 
+#define NA      0xFFFFFFFF
+
 enum
 {
     RAZER_MODE_DIRECT,
@@ -30,7 +32,7 @@ public:
     void        SetupZones();
 
     void        ResizeZone(int zone, int new_size);
-    
+
     void        DeviceUpdateLEDs();
     void        UpdateZoneLEDs(int zone);
     void        UpdateSingleLED(int led);

commit 07f55095d54731fa24471007433533608a73f001

Adjusting the Razer Huntsman V2 key layout to resolve #2673
* Adding `huntsman_v2_device` to device list
* Regression bug introduce with 942a842a

Controllers/RazerController/RazerDevices.h
@@ -3616,7 +3616,7 @@ static const razer_device huntsman_v2_device =
     RAZER_HUNTSMAN_V2_PID,
     DEVICE_TYPE_KEYBOARD,
     true,
-    9,
+    6,
     22,
     {
         &huntsman_v2_keyboard_zone,
@@ -8217,6 +8217,7 @@ static const razer_device* device_list[] =
     &huntsman_elite_device,
     &huntsman_mini_device,
     &huntsman_te_device,
+    &huntsman_v2_device,
     &huntsman_v2_analog_device,
     &huntsman_v2_tkl_device,
     &ornata_chroma_device,