8f3ed4c8694551fdfac055d8c841eb912c908459
Chris <galdarian@gmail.com>
2022-08-22 16:32:13 +1000

    Initial commit for the ASUS Strix RTX 3070 O8G V2 White GPU to resolve #2697
    + Add id to pci_ids.h
    + Register detector in ENESMBusControllerDetect.cpp
    + Link to [webpage](https://rog.asus.com/us/graphics-cards/graphics-cards/rog-strix/rog-strix-rtx3070-o8g-white-model/)

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -182,7 +182,8 @@
 #define ASUS_TUF_RTX_3060_TI_O8G_OC_V2                  0x8827
 #define ASUS_ROG_STRIX_RTX_3070_OC                      0x87B8
 #define ASUS_ROG_STRIX_RTX_3070_O8G_WHITE               0x87E0
-#define ASUS_ROG_STRIX_RTX_3070_08G_V2_GAMING           0x882C
+#define ASUS_ROG_STRIX_RTX_3070_O8G_V2_GAMING           0x882C
+#define ASUS_ROG_STRIX_RTX_3070_O8G_V2_WHITE            0x8832
 #define ASUS_KO_RTX_3070_O8G_GAMING                     0x8842
 #define ASUS_TUF_RTX_3070_O8G_GAMING                    0x87C1
 #define ASUS_TUF_RTX_3070_O8G_V2_GAMING                 0x8825
