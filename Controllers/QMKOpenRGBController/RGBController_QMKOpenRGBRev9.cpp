75f3f3a3cb66682a83b5a29be116d81630470314
Adam Honse <calcprogrammer1@gmail.com>
2022-09-23 18:01:34 -0500

    Fix warnings in RGBController_QMKOpenRGBRev9.cpp

--- Controllers/QMKOpenRGBController/RGBController_QMKOpenRGBRev9.cpp
+++ Controllers/QMKOpenRGBController/RGBController_QMKOpenRGBRev9.cpp
@@ -489,7 +489,7 @@ unsigned int RGBController_QMKOpenRGBRev9::CalculateDivisor
     (
     std::vector<point_t> led_points,
     std::set<int> rows,
-    std::set<int> columns
+    std::set<int> /*columns*/
     )
 {
     std::vector< std::vector<point_t> > row_points(rows.size());
@@ -522,7 +522,7 @@ unsigned int RGBController_QMKOpenRGBRev9::CalculateDivisor
     }
 
     unsigned int divisor = distances[0];
-    for(const std::pair<int, int> &i : counts)
+    for(const std::pair<const int, int> &i : counts)
     {
         if(counts[divisor] < i.second)
         {
@@ -559,7 +559,7 @@ void RGBController_QMKOpenRGBRev9::CountKeyTypes
 void RGBController_QMKOpenRGBRev9::PlaceLEDsInMaps
     (
         std::set<int>               unique_rows,
-        std::set<int>               unique_cols,
+        std::set<int>               /*unique_cols*/,
         unsigned int                divisor,
         std::vector<point_t>        led_points,
         std::vector<unsigned int>   led_flags,
@@ -572,7 +572,6 @@ void RGBController_QMKOpenRGBRev9::PlaceLEDsInMaps
 
     unsigned int x                  = 0;
     unsigned int y                  = 0;
-    unsigned int openrgb_idx        = 0;
     unsigned int underglow_counter  = 0;
 
     for(unsigned int i = 0; i < controller->GetTotalNumberOfLEDs(); i++)
@@ -649,7 +648,7 @@ void RGBController_QMKOpenRGBRev9::CleanMatrixMaps
         can_break               = false;
         can_break_udg           = false;
 
-        for(std::size_t j = matrix_map[i].size() - 1; j --> 0; )
+        for(int j = matrix_map[i].size() - 1; j --> 0; )
         {
             if(matrix_map[i][j] != NO_LED && width < (j + 1) && !can_break)
             {
