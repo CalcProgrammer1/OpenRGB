f4f97c61ea564775d0c68ffa065c6268696adbb3
Adam Honse <calcprogrammer1@gmail.com>
2022-09-29 11:47:58 -0500

    Add ASUS TUF RX 6800XT O16 GAMING to ENESMBusController

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -214,6 +214,7 @@
 #define ASUS_ROG_STRIX_RTX_3090_O24G_GAMING_WHITE       0x87D9
 #define ASUS_TUF_RTX_3090_O24G                          0x87B5
 #define ASUS_TUF_RTX_3090_O24G_OC                       0x87B3
+#define ASUS_TUF_RX_6800XT_O16G_GAMING                  0x04F2
 #define ASUS_TUF_RX6900XT_O16G_GAMING                   0x04F2
 
 
