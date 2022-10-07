c5cad073193b10c4cec6d58cda1963eb90c0fbd0
Teemu Linkoaho <temexter@temexter.net>
2022-10-07 21:48:51 +0000

    Fixed AMD Radeon RX 6900 XT device ID.

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -218,6 +218,7 @@
 #define ASUS_TUF_RTX_3090_O24G                          0x87B5
 #define ASUS_TUF_RTX_3090_O24G_OC                       0x87B3
 #define ASUS_TUF_RX_6800XT_O16G_GAMING                  0x04F2
+#define ASUS_TUF_RX_6900XT_O16G_GAMING                  0x04FA
 
 /*-----------------------------------------------------*\
 | EVGA Sub-Device IDs                                   |
