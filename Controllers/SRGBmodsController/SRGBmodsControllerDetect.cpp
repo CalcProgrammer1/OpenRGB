530d66732824e48ac5d165695bf6d9dc8df6b78f
Adam Honse <calcprogrammer1@gmail.com>
2022-11-26 21:25:58 -0600

    Add second SRGBMods Pi Pico string for updated code

--- Controllers/SRGBmodsController/SRGBmodsControllerDetect.cpp
+++ Controllers/SRGBmodsController/SRGBmodsControllerDetect.cpp
@@ -18,7 +18,7 @@
 
 void DetectSRGBmodsControllers(hid_device_info* info, const std::string& name)
 {
-    hid_device* dev = hid_open_path(info->path);    
+    hid_device* dev = hid_open_path(info->path);
 
     if(dev)
     {
@@ -26,12 +26,12 @@ void DetectSRGBmodsControllers(hid_device_info* info, const std::string& name)
         hid_get_product_string(dev, product, 128);
 
         std::wstring product_str(product);
-        
+
         /*-------------------------------------------------------------------------*\
         | Test the product string in case this USB ID is reused for other Pi Pico   |
         | projects                                                                  |
         \*-------------------------------------------------------------------------*/
-        if(product_str == L"SRGBmods Pico LED Controller")
+        if(product_str == L"SRGBmods Pico LED Controller" || product_str == L"Pico LED Controller")
         {
             SRGBmodsPicoController*     controller     = new SRGBmodsPicoController(dev, info->path);
             RGBController_SRGBmodsPico* rgb_controller = new RGBController_SRGBmodsPico(controller);
