11ce757ae3b49b3f939f161ec3327f09d2826383
flora <aethernali.live@gmail.com>
2023-01-19 20:43:51 +0100

    Support for TUF RX 6950XT

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -241,6 +241,7 @@
 #define ASUS_TUF_RX_6800XT_O16G_GAMING                  0x04F2
 #define ASUS_TUF_RX_6900XT_O16G_GAMING                  0x04FA
 #define ASUS_TUF_RX_6900XT_T16G_GAMING                  0x04FE
+#define ASUS_TUF_RX_6950XT_016G_GAMING                  0x0504
 #define ASUS_ROG_STRIX_LC_RX_6950XT_O16G_GAMING         0x05E9
 #define ASUS_TUF_RTX_4070TI_12G_GAMING                  0x88A4
 #define ASUS_TUF_RTX_4090_O24G_OC_GAMING                0x889A
