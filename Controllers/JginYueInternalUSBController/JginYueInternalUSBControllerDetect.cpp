e27b75c666547faca2d3b8293ed538b93fc1fe5f
tcr020 <u202115206@hust.edu.cn>
2023-07-30 09:38:46 +0800

    23/7/30 first release

--- Controllers/JginYueInternalUSBController/JginYueInternalUSBControllerDetect.cpp
+++ Controllers/JginYueInternalUSBController/JginYueInternalUSBControllerDetect.cpp
@@ -8,3 +8,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <hidapi/hidapi.h>
+
+/*---------------------------------------------------------*\
+| JGINYUE vendor ID                                         |
+\*---------------------------------------------------------*/
+#define JGINYUE_VID                                 0x0416
+
+/*---------------------------------------------------------*\
+| JGINYUE product ID                                         |
+\*---------------------------------------------------------*/
+#define JGINYUE_MOTHERBOARD_PID                     0x0125
+
+void DetectJginYueInternalUSBController(hid_device_info* info,const std::string& /*name*/)
+{
+    hid_device* dev = hid_open_path(info->path);
+
+    if(dev)
+    {
+        JginYueInternalUSBController*       controller      =new JginYueInternalUSBController(dev,info->path);
+        RGBController_JginYueInternalUSB*   rgb_controller  =new RGBController_JginYueInternalUSB(controller);
+        ResourceManager::get()->RegisterRGBController(rgb_controller);
+    }
+}
+
+REGISTER_HID_DETECTOR("JginYue Internal USB Controller",DetectJginYueInternalUSBController,JGINYUE_VID,JGINYUE_MOTHERBOARD_PID);
