acf12dfd5e73d77fd384c52abfab31ccf91ce946
Boom-Hacker <darksun0031@gmail.com>
2023-10-07 21:38:53 +0800

    Added Sapphire Nitro+ RX 7900 GRE support

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -675,6 +675,7 @@
 #define SAPPHIRE_NAVI21_TOXIC_SUB_DEV                   0xF440
 #define SAPPHIRE_NAVI21_TOXIC_SUB_DEV1                  0xF441
 #define SAPPHIRE_NAVI31_NITRO_PLUS_SUB_DEV              0xE471
+#define SAPPHIRE_NAVI31_GRE_NITRO_PLUS_SUB_DEV          0xE475
 #define SAPPHIRE_NAVI32_NITRO_PLUS_SUB_DEV              0xD475
 
 /*-----------------------------------------------------*\
