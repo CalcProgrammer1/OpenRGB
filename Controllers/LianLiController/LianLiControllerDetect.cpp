CalcProgrammer1/OpenRGB

commit dbeabe613d169d0f56f9892180cf6246aea0be39

Add support for Lian Li GAII Trinity AIO

Controllers/LianLiController/LianLiControllerDetect.cpp
@@ -34,11 +34,14 @@
 #include "RGBController_LianLiUniHubSLV2.h"
 #include "LianLiUniHubSLInfinityController.h"
 #include "RGBController_LianLiUniHubSLInfinity.h"
+#include "LianLiGAIITrinityController.h"
+#include "RGBController_LianLiGAIITrinity.h"
 
 /*-----------------------------------------------------*\
-| ENE USB vendor ID                                     |
+| USB vendor IDs                                        |
 \*-----------------------------------------------------*/
 #define ENE_USB_VID                                 0x0CF2
+#define NUVOTON_USB_VID                             0x0416
 
 /*-----------------------------------------------------*\
 | Keyboard product IDs                                  |
@@ -53,6 +56,7 @@
 #define UNI_HUB_SLINF_PID                           0xA102
 #define UNI_HUB_SLV2_PID                            0xA103
 #define UNI_HUB_SLV2_V05_PID                        0xA105
+#define GAII_USB_PID                                0x7373
 
 /*----------------------------------------------------------------------------*\
 | The Uni Hub is controlled by sending control transfers to various wIndex     |
@@ -64,7 +68,7 @@ void DetectLianLiUniHub()
 {
     libusb_device** devices = nullptr;
 
-    int ret;
+    ssize_t ret;
 
     ret = libusb_init(NULL);
     if(ret < 0)
@@ -78,7 +82,7 @@ void DetectLianLiUniHub()
         return;
     }
 
-    int deviceCount = ret;
+    ssize_t deviceCount = ret;
 
     for(int i = 0; i < deviceCount; i++)
     {
@@ -110,7 +114,7 @@ void DetectLianLiUniHub_AL10()
 {
     libusb_device** devices = nullptr;
 
-    int ret;
+    ssize_t ret;
 
     ret = libusb_init(NULL);
     if(ret < 0)
@@ -124,7 +128,7 @@ void DetectLianLiUniHub_AL10()
         return;
     }
 
-    int deviceCount = ret;
+    ssize_t deviceCount = ret;
 
     for(int i = 0; i < deviceCount; i++)
     {
@@ -221,6 +225,18 @@ void DetectStrimerControllers(hid_device_info* info, const std::string& name)
     }
 }
 
+void DetectLianLiGAIITrinity(hid_device_info* info, const std::string& /*name*/)
+{
+    hid_device* dev = hid_open_path(info->path);
+
+    if(dev)
+    {
+        LianLiGAIITrinityController*     controller     = new LianLiGAIITrinityController(dev);
+        RGBController_LianLiGAIITrinity* rgb_controller = new RGBController_LianLiGAIITrinity(controller);
+        ResourceManager::get()->RegisterRGBController(rgb_controller);
+    }
+}
+
 REGISTER_DETECTOR("Lian Li Uni Hub", DetectLianLiUniHub);
 REGISTER_HID_DETECTOR_IPU("Lian Li Uni Hub - AL", DetectLianLiUniHubAL,    ENE_USB_VID,  UNI_HUB_AL_PID,           0x01,  0xFF72, 0xA1);
 REGISTER_HID_DETECTOR_IPU("Lian Li Uni Hub - SL V2", DetectLianLiUniHubSLV2,    ENE_USB_VID,  UNI_HUB_SLV2_PID,           0x01,  0xFF72, 0xA1);
@@ -233,3 +249,5 @@ REGISTER_HID_DETECTOR_IPU("Lian Li Uni Hub - SL Infinity", DetectLianLiUniHubSLI
 \*---------------------------------------------------------------------------------------------------------*/
 
 REGISTER_HID_DETECTOR_IPU("Strimer L Connect",   DetectStrimerControllers,  ENE_USB_VID,  STRIMER_L_CONNECT_PID,      1,  0xFF72, 0xA1);
+
+REGISTER_HID_DETECTOR_I("Lian Li GA II Trinity", DetectLianLiGAIITrinity, NUVOTON_USB_VID, GAII_USB_PID, 0x02);

Controllers/LianLiController/LianLiGAIITrinityController.cpp
@@ -1,0 +1,298 @@
+/*-----------------------------------------*\
+|  LianLiGAIITrinityController.cpp          |
+|                                           |
+|  Controller for Lian Li GAII Trinity      |
+|                                           |
+|  Michael Losert 2023-10-27                |
+\*-----------------------------------------*/
+
+#include "LianLiGAIITrinityController.h"
+#include <algorithm>
+#include <cstring>
+#include <iomanip>
+#include <sstream>
+
+LianLiGAIITrinityController::LianLiGAIITrinityController(hid_device* dev_handle)
+{
+    dev = dev_handle;
+}
+
+LianLiGAIITrinityController::~LianLiGAIITrinityController()
+{
+    if(dev)
+    {
+        hid_close(dev);
+    }
+}
+
+LianLiGAIITrinityController::GAII_Info LianLiGAIITrinityController::GetControllerInfo()
+{
+    GAII_Info controllerInfo;
+
+    hid_device_info* hidInfo = hid_get_device_info(dev);
+
+    // get device location
+    controllerInfo.location = "HID: " + std::string(hidInfo->path);
+
+    // get serial number
+    std::wstring serialWStr(hidInfo->serial_number);
+    controllerInfo.serial = std::string(serialWStr.begin(), serialWStr.end());
+
+    // get firmware version
+    unsigned char data[64] = "";
+    data[0x00] = 0x01;
+    data[GAII_ByteAddress::BA_PACKET_TYPE] = GAII_PacketType::PT_FIRMWARE_INFO;
+    hid_write(dev, data, sizeof(data));
+    memset(data, 0, sizeof(data));
+    hid_read(dev, data, sizeof(data));
+    data[sizeof(data) - 1] = 0;
+    std::string response(reinterpret_cast<char*>(&data[0x06]));
+    memset(data, 0, sizeof(data));
+    hid_read(dev, data, sizeof(data));
+    data[sizeof(data) - 1] = 0;
+    response += " (" + std::string(reinterpret_cast<char*>(&data[0x06])) + ")";
+    std::replace( response.begin(), response.end(), ',', ' ');
+    controllerInfo.version = response.substr(0, 100);
+
+    return controllerInfo;
+}
+
+unsigned char* LianLiGAIITrinityController::GetRGBControlPacketTemplate()
+{
+    static unsigned char usb_buf[64];
+    memset(usb_buf, 0, sizeof(usb_buf));
+
+    usb_buf[0x00] = 0x01;
+    usb_buf[GAII_ByteAddress::BA_PACKET_TYPE] = GAII_PacketType::PT_RGB_CONTROL;
+    usb_buf[0x05] = 0x13;
+
+    return usb_buf;
+}
+
+void LianLiGAIITrinityController::SetRGB(unsigned char* usb_buf, RGBColor* rgb0, RGBColor* rgb1, RGBColor* rgb2, RGBColor* rgb3)
+{
+    if(rgb0)
+    {
+        usb_buf[GAII_ByteAddress::BA_R0] = RGBGetRValue(*rgb0);
+        usb_buf[GAII_ByteAddress::BA_G0] = RGBGetGValue(*rgb0);
+        usb_buf[GAII_ByteAddress::BA_B0] = RGBGetBValue(*rgb0);
+    }
+
+    if(rgb1)
+    {
+        usb_buf[GAII_ByteAddress::BA_R1] = RGBGetRValue(*rgb1);
+        usb_buf[GAII_ByteAddress::BA_G1] = RGBGetGValue(*rgb1);
+        usb_buf[GAII_ByteAddress::BA_B1] = RGBGetBValue(*rgb1);
+    }
+
+    if(rgb2)
+    {
+        usb_buf[GAII_ByteAddress::BA_R2] = RGBGetRValue(*rgb2);
+        usb_buf[GAII_ByteAddress::BA_G2] = RGBGetGValue(*rgb2);
+        usb_buf[GAII_ByteAddress::BA_B2] = RGBGetBValue(*rgb2);
+    }
+
+    if(rgb3)
+    {
+        usb_buf[GAII_ByteAddress::BA_R3] = RGBGetRValue(*rgb3);
+        usb_buf[GAII_ByteAddress::BA_G3] = RGBGetGValue(*rgb3);
+        usb_buf[GAII_ByteAddress::BA_B3] = RGBGetBValue(*rgb3);
+    }
+}
+
+void LianLiGAIITrinityController::SetMode_Rainbow(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_RAINBOW;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    usb_buf[GAII_ByteAddress::BA_DIRECTION] = direction;
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_RainbowMorph(GAII_Brightness brightness, GAII_Speed speed)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_RAINBOW_MORPH;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_StaticColor(GAII_Brightness brightness, RGBColor rgb0, RGBColor rgb1)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_STATIC_COLOR;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    SetRGB(usb_buf, rgb0, rgb1);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_BreathingColor(GAII_Brightness brightness, GAII_Speed speed, RGBColor rgb0, RGBColor rgb1)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_BREATHING_COLOR;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    SetRGB(usb_buf, rgb0, rgb1);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_Runway(GAII_Brightness brightness, GAII_Speed speed, RGBColor rgb0, RGBColor rgb1)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_RUNWAY;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    SetRGB(usb_buf, rgb0, rgb1);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_Meteor(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction, RGBColor rgb0, RGBColor rgb1, RGBColor rgb2, RGBColor rgb3)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_METEOR;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    usb_buf[GAII_ByteAddress::BA_DIRECTION] = direction;
+    SetRGB(usb_buf, rgb0, rgb1, rgb2, rgb3);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_Vortex(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction, RGBColor rgb0, RGBColor rgb1, RGBColor rgb2, RGBColor rgb3)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_VORTEX;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    usb_buf[GAII_ByteAddress::BA_DIRECTION] = direction;
+    SetRGB(usb_buf, rgb0, rgb1, rgb2, rgb3);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_CrossingOver(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction, RGBColor rgb0, RGBColor rgb1, RGBColor rgb2, RGBColor rgb3)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_CROSSING_OVER;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    usb_buf[GAII_ByteAddress::BA_DIRECTION] = direction;
+    SetRGB(usb_buf, rgb0, rgb1, rgb2, rgb3);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_TaiChi(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction, RGBColor rgb0, RGBColor rgb1)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_TAI_CHI;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    usb_buf[GAII_ByteAddress::BA_DIRECTION] = direction;
+    SetRGB(usb_buf, rgb0, rgb1);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_ColorfulStarryNight(GAII_Brightness brightness, GAII_Speed speed)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_COLORFUL_STARRY_NIGHT;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_StaticStarryNight(GAII_Brightness brightness, GAII_Speed speed, RGBColor rgb0)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_STATIC_STARRY_NIGHT;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    SetRGB(usb_buf, rgb0);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_Voice(GAII_Brightness brightness, GAII_Speed speed, RGBColor rgb0, RGBColor rgb1)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_VOICE;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    SetRGB(usb_buf, rgb0, rgb1);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_BigBang(GAII_Brightness brightness, GAII_Speed speed, RGBColor rgb0, RGBColor rgb1, RGBColor rgb2, RGBColor rgb3)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_BIG_BANG;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    SetRGB(usb_buf, rgb0, rgb1, rgb2, rgb3);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_Pump(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction, RGBColor rgb0, RGBColor rgb1)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_PUMP;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    usb_buf[GAII_ByteAddress::BA_DIRECTION] = direction;
+    SetRGB(usb_buf, rgb0, rgb1);
+
+    hid_write(dev, usb_buf, 64);
+}
+
+void LianLiGAIITrinityController::SetMode_ColorsMorph(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction)
+{
+    unsigned char *usb_buf = GetRGBControlPacketTemplate();
+
+    usb_buf[GAII_ByteAddress::BA_MODE] = GAII_Modes::M_COLORS_MORPH;
+    usb_buf[GAII_ByteAddress::BA_RING] = GAII_Ring::R_BOTH;
+    usb_buf[GAII_ByteAddress::BA_BRIGHTNESS] = brightness;
+    usb_buf[GAII_ByteAddress::BA_SPEED] = speed;
+    usb_buf[GAII_ByteAddress::BA_DIRECTION] = direction;
+
+    hid_write(dev, usb_buf, 64);
+}

Controllers/LianLiController/LianLiGAIITrinityController.h
@@ -1,0 +1,146 @@
+/*-----------------------------------------*\
+|  LianLiGAIITrinityController.h            |
+|                                           |
+|  Controller for Lian Li GAII Trinity      |
+|                                           |
+|  Michael Losert 2023-10-27                |
+\*-----------------------------------------*/
+
+#include "RGBController.h"
+#include "hidapi/hidapi.h"
+#include <vector>
+#ifdef __FreeBSD__
+#include <libusb.h>
+#else
+#include <libusb-1.0/libusb.h>
+#endif
+
+#pragma once
+
+/*----------------------------------------------------------------------------*\
+| Definitions related to LED configuration.                                    |
+\*----------------------------------------------------------------------------*/
+
+class LianLiGAIITrinityController
+{
+public:
+    struct GAII_Info
+    {
+        std::string location;
+        std::string serial;
+        std::string version;
+    };
+
+    enum GAII_PacketType : unsigned char
+    {
+        PT_RGB_CONTROL = 0x83,
+        PT_FIRMWARE_INFO = 0x86
+    };
+
+    enum GAII_Modes : unsigned char
+    {
+        M_RAINBOW = 0x01,
+        M_RAINBOW_MORPH,
+        M_STATIC_COLOR,
+        M_BREATHING_COLOR,
+        M_RUNWAY,
+        M_METEOR,
+        M_VORTEX,
+        M_CROSSING_OVER,
+        M_TAI_CHI,
+        M_COLORFUL_STARRY_NIGHT,
+        M_STATIC_STARRY_NIGHT,
+        M_VOICE,
+        M_BIG_BANG,
+        M_PUMP,
+        M_COLORS_MORPH,
+        /* M_BOUNCE, */ // TODO: requires zone-specific modes
+    };
+
+    enum GAII_Ring : unsigned char
+    {
+        R_INNER,
+        R_OUTER,
+        R_BOTH,
+    };
+
+    enum GAII_Brightness : unsigned char
+    {
+        B_OFF,
+        B_25,
+        B_50,
+        B_75,
+        B_100,
+    };
+
+    enum GAII_Speed : unsigned char
+    {
+        S_VERY_SLOW,
+        S_SLOW,
+        S_MODERATE,
+        S_FAST,
+        S_VERY_FAST,
+    };
+
+    enum GAII_Direction : unsigned char
+    {
+        D_RIGHT,
+        D_LEFT
+    };
+
+    enum GAII_ByteAddress : unsigned char
+    {
+        BA_PACKET_TYPE = 0x01,
+
+        BA_RING = 0x06,
+        BA_MODE,
+        BA_BRIGHTNESS,
+        BA_SPEED,
+
+        BA_R0 = 0x0A,
+        BA_G0,
+        BA_B0,
+        BA_R1,
+        BA_G1,
+        BA_B1,
+        BA_R2,
+        BA_G2,
+        BA_B2,
+        BA_R3,
+        BA_G3,
+        BA_B3,
+
+        BA_DIRECTION = 0x16,
+    };
+
+    LianLiGAIITrinityController(hid_device* dev_handle);
+    ~LianLiGAIITrinityController();
+
+    GAII_Info GetControllerInfo();
+
+    void SetMode_Rainbow(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction);
+    void SetMode_RainbowMorph(GAII_Brightness brightness, GAII_Speed speed);
+    void SetMode_StaticColor(GAII_Brightness brightness, RGBColor rgb0, RGBColor rgb1);
+    void SetMode_BreathingColor(GAII_Brightness brightness, GAII_Speed speed, RGBColor rgb0, RGBColor rgb1);
+    void SetMode_Runway(GAII_Brightness brightness, GAII_Speed speed, RGBColor rgb0, RGBColor rgb1);
+    void SetMode_Meteor(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction, RGBColor rgb0, RGBColor rgb1, RGBColor rgb2, RGBColor rgb3);
+    void SetMode_Vortex(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction, RGBColor rgb0, RGBColor rgb1, RGBColor rgb2, RGBColor rgb3);
+    void SetMode_CrossingOver(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction, RGBColor rgb0, RGBColor rgb1, RGBColor rgb2, RGBColor rgb3);
+    void SetMode_TaiChi(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction, RGBColor rgb0, RGBColor rgb1);
+    void SetMode_ColorfulStarryNight(GAII_Brightness brightness, GAII_Speed speed);
+    void SetMode_StaticStarryNight(GAII_Brightness brightness, GAII_Speed speed, RGBColor rgb0);
+    void SetMode_Voice(GAII_Brightness brightness, GAII_Speed speed, RGBColor rgb0, RGBColor rgb1);
+    void SetMode_BigBang(GAII_Brightness brightness, GAII_Speed speed, RGBColor rgb0, RGBColor rgb1, RGBColor rgb2, RGBColor rgb3);
+    void SetMode_Pump(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction, RGBColor rgb0, RGBColor rgb1);
+    void SetMode_ColorsMorph(GAII_Brightness brightness, GAII_Speed speed, GAII_Direction direction);
+
+private:
+    unsigned char* GetRGBControlPacketTemplate();
+    void SetRGB(unsigned char* usb_buf, RGBColor rgb0) { SetRGB(usb_buf, &rgb0, nullptr, nullptr, nullptr); };
+    void SetRGB(unsigned char* usb_buf, RGBColor rgb0, RGBColor rgb1) { SetRGB(usb_buf, &rgb0, &rgb1, nullptr, nullptr); };
+    void SetRGB(unsigned char* usb_buf, RGBColor rgb0, RGBColor rgb1, RGBColor rgb2) { SetRGB(usb_buf, &rgb0, &rgb1, &rgb2, nullptr); };
+    void SetRGB(unsigned char* usb_buf, RGBColor rgb0, RGBColor rgb1, RGBColor rgb2, RGBColor rgb3) { SetRGB(usb_buf, &rgb0, &rgb1, &rgb2, &rgb3); };
+    void SetRGB(unsigned char* usb_buf, RGBColor* rgb0, RGBColor* rgb1, RGBColor* rgb2, RGBColor* rgb3);
+
+    hid_device* dev;
+};

Controllers/LianLiController/RGBController_LianLiGAIITrinity.h
@@ -1,0 +1,45 @@
+/*-----------------------------------------*\
+|  RGBController_LianLiGAIITrinity.h        |
+|                                           |
+|  Lian Li GAII Trinity AIO LED Driver      |
+|                                           |
+|  Michael Losert   2023-10-27              |
+\*-----------------------------------------*/
+
+#pragma once
+
+#include <cstdint>
+#include <vector>
+
+#include "LianLiGAIITrinityController.h"
+#include "RGBController.h"
+
+class RGBController_LianLiGAIITrinity : public RGBController
+{
+public:
+    RGBController_LianLiGAIITrinity(LianLiGAIITrinityController* controller_ptr);
+    ~RGBController_LianLiGAIITrinity();
+
+    void SetupZones();
+
+    void ResizeZone(int zone, int new_size);
+
+    void DeviceUpdateLEDs();
+    void UpdateZoneLEDs(int zone);
+    void UpdateSingleLED(int led);
+
+    void DeviceUpdateMode();
+
+    void SetCustomMode();
+
+    LianLiGAIITrinityController::GAII_Direction OpenRGBDirection2GAIIDirection(unsigned int openrgb_direction)
+    {
+        if(openrgb_direction == MODE_DIRECTION_LEFT)
+            return LianLiGAIITrinityController::GAII_Direction::D_LEFT;
+
+        return LianLiGAIITrinityController::GAII_Direction::D_RIGHT;
+    }
+
+private:
+    LianLiGAIITrinityController* controller;
+};

OpenRGB.pro
@@ -604,6 +604,8 @@ HEADERS +=                                                                      
     Controllers/LianLiController/RGBController_LianLiUniHubSLInfinity.h                         \
     Controllers/LianLiController/LianLiUniHubSLV2Controller.h                                   \
     Controllers/LianLiController/RGBController_LianLiUniHubSLV2.h                               \
+    Controllers/LianLiController/LianLiGAIITrinityController.h                                  \
+    Controllers/LianLiController/RGBController_LianLiGAIITrinity.h                              \
     Controllers/LogitechController/LogitechProtocolCommon.h                                     \
     Controllers/LogitechController/LogitechG203LController.h                                    \
     Controllers/LogitechController/LogitechG213Controller.h                                     \
@@ -1291,6 +1293,8 @@ SOURCES +=                                                                      
     Controllers/LianLiController/RGBController_LianLiUniHubSLInfinity.cpp                       \
     Controllers/LianLiController/LianLiUniHubSLV2Controller.cpp                                 \
     Controllers/LianLiController/RGBController_LianLiUniHubSLV2.cpp                             \
+    Controllers/LianLiController/LianLiGAIITrinityController.cpp                                \
+    Controllers/LianLiController/RGBController_LianLiGAIITrinity.cpp                            \
     Controllers/LogitechController/LogitechControllerDetect.cpp                                 \
     Controllers/LogitechController/LogitechProtocolCommon.cpp                                   \
     Controllers/LogitechController/LogitechG203LController.cpp                                  \

commit a689db4d3d3ed9f0f5792f530d8fb7abc53e91d5

Removed hid_get_device_info() function to correct CI/CD pipelines
* Regression introduced in dbeabe61
* Removed unrequired includes
* Aligned other variables and structures to ensure feature parity

Controllers/LianLiController/LianLiControllerDetect.cpp
@@ -233,6 +233,7 @@ void DetectLianLiGAIITrinity(hid_device_info* info, const std::string& /*name*/)
     {
         LianLiGAIITrinityController*     controller     = new LianLiGAIITrinityController(dev);
         RGBController_LianLiGAIITrinity* rgb_controller = new RGBController_LianLiGAIITrinity(controller);
+        rgb_controller->location                        = "HID: " + std::string(info->path);
         ResourceManager::get()->RegisterRGBController(rgb_controller);
     }
 }

Controllers/LianLiController/LianLiGAIITrinityController.cpp
@@ -29,13 +29,12 @@ LianLiGAIITrinityController::GAII_Info LianLiGAIITrinityController::GetControlle
 {
     GAII_Info controllerInfo;
 
-    hid_device_info* hidInfo = hid_get_device_info(dev);
-
-    // get device location
-    controllerInfo.location = "HID: " + std::string(hidInfo->path);
-
     // get serial number
-    std::wstring serialWStr(hidInfo->serial_number);
+    const uint8_t sz    = 255;
+    wchar_t       tmp[sz];
+
+    hid_get_serial_number_string(dev, tmp, sz);
+    std::wstring serialWStr = std::wstring(tmp);
     controllerInfo.serial = std::string(serialWStr.begin(), serialWStr.end());
 
     // get firmware version

Controllers/LianLiController/LianLiGAIITrinityController.h
@@ -9,11 +9,6 @@
 #include "RGBController.h"
 #include "hidapi/hidapi.h"
 #include <vector>
-#ifdef __FreeBSD__
-#include <libusb.h>
-#else
-#include <libusb-1.0/libusb.h>
-#endif
 
 #pragma once
 
@@ -26,7 +21,6 @@ class LianLiGAIITrinityController
 public:
     struct GAII_Info
     {
-        std::string location;
         std::string serial;
         std::string version;
     };

Controllers/LianLiController/RGBController_LianLiGAIITrinity.cpp
@@ -31,7 +31,6 @@ RGBController_LianLiGAIITrinity::RGBController_LianLiGAIITrinity(LianLiGAIITrini
 
     LianLiGAIITrinityController::GAII_Info controllerInfo = controller->GetControllerInfo();
     version     = controllerInfo.version;
-    location    = controllerInfo.location;
     serial      = controllerInfo.serial;
 
     mode Rainbow;