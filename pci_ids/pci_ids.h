3ab12a45b82a79f1ff25f341d20718a189e46ac2
Chris <galdarian@gmail.com>
2024-01-02 13:57:57 +1100

    Adding PNY RTX 4090 XLR8 Verto Epic-X OC
    * Added `0x13D9` to pci_ids.h
    * Registered detector in PNYLovelaceGPUControllerDetect.cpp
    * Link to [webpage](https://www.pny.com/en-eu/pny-geforce-rtx-4090-24gb-xlr8-rgb-triple-fan-oc)

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -626,6 +626,7 @@
 #define PNY_RTX_4080_XLR8_VERTO_SUB_DEV                 0x13BB
 #define PNY_RTX_4090_XLR8_VERTO_SUB_DEV                 0x13AE
 #define PNY_RTX_4090_VERTO_EPIC_X_SUB_DEV               0x13D8
+#define PNY_RTX_4090_VERTO_EPIC_X_OC_SUB_DEV            0x13D9
 
 /*-----------------------------------------------------*\
 | Palit Sub-Device IDs                                    |
