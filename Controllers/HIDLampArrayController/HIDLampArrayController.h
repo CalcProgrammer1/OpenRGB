82f54ba227e275592b6ecf62309824379df39c01
Adam Honse <calcprogrammer1@gmail.com>
2024-05-06 19:00:41 -0500

    Begin implementing the HID LampArray protocol for Arduino test device

--- Controllers/HIDLampArrayController/HIDLampArrayController.h
+++ Controllers/HIDLampArrayController/HIDLampArrayController.h
@@ -1,3 +1,14 @@
+/*---------------------------------------------------------*\
+| HIDLampArrayController.h                                  |
+|                                                           |
+|   Driver for HID LampArray Devices                        |
+|                                                           |
+|   Adam Honse (calcprogrammer1@gmail.com)      26 Mar 2024 |
+|                                                           |
+|   This file is part of the OpenRGB project                |
+|   SPDX-License-Identifier: GPL-2.0-only                   |
+\*---------------------------------------------------------*/
+
 #pragma once
 
 #include <string>
@@ -5,16 +16,57 @@
 
 #include "RGBController.h"
 
+typedef struct
+{
+    hid_device *    hid_dev_LampArray;
+    hid_device *    hid_dev_LampArrayAttributesReport;
+    hid_device *    hid_dev_LampAttributesRequestReport;
+    hid_device *    hid_dev_LampAttributesResponseReport;
+    hid_device *    hid_dev_LampMultiUpdateReport;
+    hid_device *    hid_dev_LampRangeUpdateReport;
+    hid_device *    hid_dev_LampArrayControlReport;
+} lamparray_hid_devs;
+
+enum
+{
+    HID_LAMPARRAY_KIND_UNDEFINED        = 0x00,     /* Undefined                    */
+    HID_LAMPARRAY_KIND_KEYBOARD         = 0x01,     /* LampArrayKindKeyboard        */
+    HID_LAMPARRAY_KIND_MOUSE            = 0x02,     /* LampArrayKindMouse           */
+    HID_LAMPARRAY_KIND_GAME_CONTROLLER  = 0x03,     /* LampArrayKindGameController  */
+    HID_LAMPARRAY_KIND_PERIPHERAL       = 0x04,     /* LampArrayKindPeripheral      */
+    HID_LAMPARRAY_KIND_SCENE            = 0x05,     /* LampArrayKindScene           */
+    HID_LAMPARRAY_KIND_NOTIFICATIOn     = 0x06,     /* LampArrayKindNotification    */
+    HID_LAMPARRAY_KIND_CHASSIS          = 0x07,     /* LampArrayKindChassis         */
+    HID_LAMPARRAY_KIND_WEARABLE         = 0x08,     /* LampArrayKindWearable        */
+    HID_LAMPARRAY_KIND_FURNITURE        = 0x09,     /* LampArrayKindFurniture       */
+    HID_LAMPARRAY_KIND_ART              = 0x0A,     /* LampArrayKindArt             */
+};
+
 class HIDLampArrayController
 {
 public:
-    HIDLampArrayController(hid_device *dev_handle, const char *path);
+    HIDLampArrayController(lamparray_hid_devs *devs_handle);
     ~HIDLampArrayController();
 
     std::string GetDeviceLocation();
     std::string GetSerialString();
 
+    unsigned int GetLampCount();
+
 private:
-    hid_device *dev;
+
+    void GetLampArrayAttributesReport();
+
+    lamparray_hid_devs * devs;
     std::string location;
+
+    /*-----------------------------------------------------*\
+    | Parameters from LampArrayAttributesReport             |
+    \*-----------------------------------------------------*/
+    unsigned short  lamp_count;
+    unsigned int    bounding_width;
+    unsigned int    bounding_height;
+    unsigned int    bounding_depth;
+    unsigned int    kind;
+    unsigned int    min_interval;
 };
--- Controllers/HIDLampArrayController/RGBController_HIDLampArray.cpp
+++ Controllers/HIDLampArrayController/RGBController_HIDLampArray.cpp
@@ -1,21 +1,32 @@
+/*---------------------------------------------------------*\
+| RGBController_HIDLampArray.cpp                            |
+|                                                           |
+|   RGBController for HID LampArray Devices                 |
+|                                                           |
+|   Adam Honse (calcprogrammer1@gmail.com)      26 Mar 2024 |
+|                                                           |
+|   This file is part of the OpenRGB project                |
+|   SPDX-License-Identifier: GPL-2.0-only                   |
+\*---------------------------------------------------------*/
+
 #include "RGBController_HIDLampArray.h"
 
 RGBController_HIDLampArray::RGBController_HIDLampArray(HIDLampArrayController* controller_ptr)
 {
-    controller  = controller_ptr;
+    controller          = controller_ptr;
 
-    name        = "HID LampArray Device";
-    vendor      = "Generic";
-    type        = DEVICE_TYPE_MOUSEMAT;
-    description = "HID LampArray Device";
-    location    = controller->GetDeviceLocation();
-    serial      = controller->GetSerialString();
+    name                = "HID LampArray Device";
+    vendor              = "Generic";
+    type                = DEVICE_TYPE_MOUSEMAT;
+    description         = "HID LampArray Device";
+    location            = controller->GetDeviceLocation();
+    serial              = controller->GetSerialString();
 
     mode Direct;
-    Direct.name       = "Direct";
-    Direct.value      = 0;
-    Direct.flags      = MODE_FLAG_HAS_PER_LED_COLOR;
-    Direct.color_mode = MODE_COLORS_PER_LED;
+    Direct.name         = "Direct";
+    Direct.value        = 0;
+    Direct.flags        = MODE_FLAG_HAS_PER_LED_COLOR;
+    Direct.color_mode   = MODE_COLORS_PER_LED;
     modes.push_back(Direct);
 
     SetupZones();
@@ -28,7 +39,23 @@ RGBController_HIDLampArray::~RGBController_HIDLampArray()
 
 void RGBController_HIDLampArray::SetupZones()
 {
+    zone new_zone;
+    new_zone.name       = "Test Zone";
+    new_zone.type       = ZONE_TYPE_SINGLE;
+    new_zone.leds_count = controller->GetLampCount();
+    new_zone.leds_min   = new_zone.leds_count;
+    new_zone.leds_max   = new_zone.leds_count;
+    new_zone.matrix_map = NULL;
+    zones.push_back(new_zone);
 
+    for(unsigned int led_idx = 0; led_idx < new_zone.leds_count; led_idx++)
+    {
+        led new_led;
+        new_led.name    = "Test LED";
+        leds.push_back(new_led);
+    }
+
+    SetupColors();
 }
 
 void RGBController_HIDLampArray::ResizeZone(int /*zone*/, int /*new_size*/)
