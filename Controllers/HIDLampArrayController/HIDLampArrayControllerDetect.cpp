ee4cdacd6fb1404a1cceebbde74d0e4707f7dc1d
Adam Honse <calcprogrammer1@gmail.com>
2024-05-06 19:00:41 -0500

    Begin implementing the HID LampArray protocol for Arduino test device

--- Controllers/HIDLampArrayController/HIDLampArrayControllerDetect.cpp
+++ Controllers/HIDLampArrayController/HIDLampArrayControllerDetect.cpp
@@ -1,3 +1,14 @@
+/*---------------------------------------------------------*\
+| HIDLampArrayControllerDetect.cpp                          |
+|                                                           |
+|   Detector for HID LampArray Devices                      |
+|                                                           |
+|   Adam Honse (calcprogrammer1@gmail.com)      26 Mar 2024 |
+|                                                           |
+|   This file is part of the OpenRGB project                |
+|   SPDX-License-Identifier: GPL-2.0-only                   |
+\*---------------------------------------------------------*/
+
 #include "Detector.h"
 #include "HIDLampArrayController.h"
 #include "RGBController.h"
@@ -5,15 +16,61 @@
 #include <vector>
 #include <hidapi/hidapi.h>
 
+static lamparray_hid_devs devs;
+
 void DetectHIDLampArrayControllers(hid_device_info* info, const std::string& name)
 {
     hid_device* dev = hid_open_path(info->path);
 
     if(dev)
     {
-        HIDLampArrayController*     controller     = new HIDLampArrayController(dev, info->path);
+        if(info->usage_page == 0x59 && info->usage == 0x01)
+        {
+            devs.hid_dev_LampArray = dev;
+        }
+
+        if(info->usage_page == 0x59 && info->usage == 0x02)
+        {
+            devs.hid_dev_LampArrayAttributesReport = dev;
+        }
+
+        if(info->usage_page == 0x59 && info->usage == 0x20)
+        {
+            devs.hid_dev_LampAttributesRequestReport = dev;
+        }
+
+        if(info->usage_page == 0x59 && info->usage == 0x22)
+        {
+            devs.hid_dev_LampAttributesResponseReport = dev;
+        }
+
+        if(info->usage_page == 0x59 && info->usage == 0x50)
+        {
+            devs.hid_dev_LampMultiUpdateReport = dev;
+        }
+
+        if(info->usage_page == 0x59 && info->usage == 0x60)
+        {
+            devs.hid_dev_LampRangeUpdateReport = dev;
+        }
+
+        if(info->usage_page == 0x59 && info->usage == 0x70)
+        {
+            devs.hid_dev_LampArrayControlReport = dev;
+        }
+    }
+
+    if(devs.hid_dev_LampArray
+    && devs.hid_dev_LampArrayAttributesReport
+    && devs.hid_dev_LampAttributesRequestReport
+    && devs.hid_dev_LampAttributesResponseReport
+    && devs.hid_dev_LampMultiUpdateReport
+    && devs.hid_dev_LampRangeUpdateReport
+    && devs.hid_dev_LampArrayControlReport)
+    {
+        HIDLampArrayController*     controller     = new HIDLampArrayController(&devs);
         RGBController_HIDLampArray* rgb_controller = new RGBController_HIDLampArray(controller);
-        rgb_controller->name                     = name;
+        rgb_controller->name                       = name;
 
         ResourceManager::get()->RegisterRGBController(rgb_controller);
     }
