4cd754530e1db68b6be93e8b084685e50470fe1d
EasyNetDev <devel@easynet.dev>
2024-06-17 15:15:20 +0200

    Fix the confusing warning: ERROR! Source file debian/changelog.in missing.

--- scripts/build-package-files.sh
+++ scripts/build-package-files.sh
@@ -10,8 +10,9 @@ fi
 
 INFILE_PATH=${1}${INFILE_SUFFIX}
 
-if [ -e ${INFILE_PATH} ]; then
+if [ ! -e ${INFILE_PATH} ]; then
     echo "ERROR! Source file ${INFILE_PATH} missing."
+    exit 1
 fi
 
 if [ -e ${1} ]; then
