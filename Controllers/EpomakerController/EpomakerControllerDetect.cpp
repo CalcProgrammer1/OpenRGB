357b236fae175f348bba1c6a52530815b7484385
Gabriel de Souza <gabrielmonteirodesouza@tutanota.com>
2024-06-17 11:41:49 -0300

    Add Attack Shark k86 keyboard

--- Controllers/EpomakerController/EpomakerControllerDetect.cpp
+++ Controllers/EpomakerController/EpomakerControllerDetect.cpp
@@ -17,8 +17,10 @@
 
 #define EPOMAKER_VID                    0x3151
 #define EPOMAKER_TH80_Pro_USB_PID       0x4010
-#define EPOMAKER_TH80_Pro_Dongle_PID    0x4011
+#define ATTACKSHARK_K86_USB_PID         0x4015
+#define EPOMAKER_TH80_Pro_Dongle_PID    0x4011 /* Attack shark's Dongle is the same. */
 #define EPOMAKER_TH80_Pro_BT_PID        0x4013
+#define ATTACKSHARK_K86_BT_PID          0x4012
 
 /******************************************************************************************\
 *                                                                                          *
@@ -43,8 +45,10 @@ void DetectEpomakerControllers(hid_device_info* info, const std::string&)
 
 REGISTER_HID_DETECTOR_I("Epomaker TH80 Pro (USB Cable)", DetectEpomakerControllers, EPOMAKER_VID, EPOMAKER_TH80_Pro_USB_PID, 2);
 REGISTER_HID_DETECTOR_I("Epomaker TH80 Pro (USB Dongle)", DetectEpomakerControllers, EPOMAKER_VID, EPOMAKER_TH80_Pro_Dongle_PID, 2);
+REGISTER_HID_DETECTOR_I("Attack Shark K86 (USB Cable)", DetectEpomakerControllers, EPOMAKER_VID, ATTACKSHARK_K86_USB_PID, 2);
 
 /*---------------------------------------------------------*\
 | Bluetooth Not implemented                                 |
 \*---------------------------------------------------------*/
 //REGISTER_HID_DETECTOR("Epomaker TH80 Pro (Bluetooth)", DetectEpomakerControllers, EPOMAKER_VID, EPOMAKER_TH80_Pro_BT_PID);
+//REGISTER_HID_DETECTOR("Attack Shark K86 (Bluetooth)", DetectEpomakerControllers, EPOMAKER_VID, ATTACKSHARK_K86_BT_PID);
