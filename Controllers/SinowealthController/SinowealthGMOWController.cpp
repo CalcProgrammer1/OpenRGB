aeca035fa3abdb46ef3b077faed973a8b4668540
Adam Honse <calcprogrammer1@gmail.com>
2024-07-29 22:58:03 -0500

    Fix wstring to string conversion warnings in SinowealthGMOWController.cpp

--- Controllers/SinowealthController/SinowealthGMOWController.cpp
+++ Controllers/SinowealthController/SinowealthGMOWController.cpp
@@ -6,11 +6,11 @@
 |  Matt Silva (thesilvanator) 05/2022         |
 \*-------------------------------------------*/
 
-#include "SinowealthGMOWController.h"
 #include <cstring>
-#include <LogManager.h>
-
 #include <iostream>
+#include "LogManager.h"
+#include "SinowealthGMOWController.h"
+#include "StringUtils.h"
 
 SinowealthGMOWController::SinowealthGMOWController(hid_device* dev_handle, char *_path, int _type)
 {
@@ -56,10 +56,7 @@ std::string SinowealthGMOWController::GetSerialString()
         return("");
     }
 
-    std::wstring return_wstring = serial_string;
-    std::string return_string(return_wstring.begin(), return_wstring.end());
-
-    return(return_string);
+    return(StringUtils::wstring_to_string(serial_string));
 }
 
 std::string SinowealthGMOWController::GetFirmwareVersion()
