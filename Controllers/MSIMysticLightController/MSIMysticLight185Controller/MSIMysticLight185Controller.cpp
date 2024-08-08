ed860467b8cc8ad2e1854f0d38dce06e771177fe
Adam Honse <calcprogrammer1@gmail.com>
2024-08-07 21:43:32 -0500

    Fix wstring to string conversion warning in MSIMysticLight185Controller.cpp

--- Controllers/MSIMysticLightController/MSIMysticLight185Controller/MSIMysticLight185Controller.cpp
+++ Controllers/MSIMysticLightController/MSIMysticLight185Controller/MSIMysticLight185Controller.cpp
@@ -1052,10 +1052,9 @@ void MSIMysticLight185Controller::ReadName()
     hid_get_manufacturer_string(dev, tname, 256);
 
     /*-----------------------------------------------------*\
-    | Convert wchar_t into std::wstring into std::string    |
+    | Convert to std::string                                |
     \*-----------------------------------------------------*/
-    std::wstring wname = std::wstring(tname);
-    name = std::string(wname.begin(), wname.end());
+    name = StringUtils::wstring_to_string(tname);
 
     /*-----------------------------------------------------*\
     | Get the product string from HID                       |
@@ -1065,8 +1064,7 @@ void MSIMysticLight185Controller::ReadName()
     /*-----------------------------------------------------*\
     | Append the product string to the manufacturer string  |
     \*-----------------------------------------------------*/
-    wname = std::wstring(tname);
-    name.append(" ").append(std::string(wname.begin(), wname.end()));
+    name.append(" ").append(StringUtils::wstring_to_string(tname));
 }
 
 MSI_MODE MSIMysticLight185Controller::GetMode()
