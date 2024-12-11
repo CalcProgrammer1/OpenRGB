6e9dd4c83bbdcb8e5f05776fecff48ec9879e29f
Adam Honse <calcprogrammer1@gmail.com>
2024-12-10 21:20:27 -0600

    Clean up SPDAccessor, split out classes into their own files, add comments, update coding style

---
+++ SPDAccessor/DDR5DirectAccessor.cpp
@@ -0,0 +1,122 @@
+/*---------------------------------------------------------*\
+| DDR5DirectAccessor.cpp                                    |
+|                                                           |
+|   DDR5 SPD accessor implementation using direct i2c       |
+|                                                           |
+|   This file is part of the OpenRGB project                |
+|   SPDX-License-Identifier: GPL-2.0-only                   |
+\*---------------------------------------------------------*/
+
+#include "DDR5DirectAccessor.h"
+#include "LogManager.h"
+
+using namespace std::chrono_literals;
+
+DDR5DirectAccessor::DDR5DirectAccessor(i2c_smbus_interface *bus, uint8_t spd_addr)
+  : DDR5Accessor(bus, spd_addr)
+{
+}
+
+DDR5DirectAccessor::~DDR5DirectAccessor()
+{
+}
+
+bool DDR5DirectAccessor::isAvailable(i2c_smbus_interface *bus, uint8_t spd_addr)
+{
+    bool retry = true;
+
+    while(true)
+    {
+        std::this_thread::sleep_for(SPD_IO_DELAY);
+        int ddr5Magic = bus->i2c_smbus_read_byte_data(spd_addr, 0x00);
+        std::this_thread::sleep_for(SPD_IO_DELAY);
+        int ddr5Sensor = bus->i2c_smbus_read_byte_data(spd_addr, 0x01);
+        std::this_thread::sleep_for(SPD_IO_DELAY);
+
+        if(ddr5Magic < 0 || ddr5Sensor < 0)
+        {
+            break;
+        }
+
+        LOG_TRACE("SPD Hub Magic: 0x%02x 0x%02x", ddr5Magic, ddr5Sensor);
+
+        if(ddr5Magic == 0x51 && (ddr5Sensor & 0xEF) == 0x08)
+        {
+            return true;
+        }
+
+        int page = bus->i2c_smbus_read_byte_data(spd_addr, SPD_DDR5_MREG_VIRTUAL_PAGE);
+        std::this_thread::sleep_for(SPD_IO_DELAY);
+
+        LOG_TRACE("SPD Page: 0x%02x", page);
+        if(page < 0)
+        {
+            break;
+        }
+        else if(retry && page > 0 && page < (SPD_DDR5_EEPROM_LENGTH >> SPD_DDR5_EEPROM_PAGE_SHIFT))
+        {
+            // This still might be a DDR5 module, just the page is off
+            bus->i2c_smbus_write_byte_data(spd_addr, SPD_DDR5_MREG_VIRTUAL_PAGE, 0);
+            std::this_thread::sleep_for(SPD_IO_DELAY);
+            retry = false;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return false;
+}
+
+SPDAccessor *DDR5DirectAccessor::copy()
+{
+    DDR5DirectAccessor *access = new DDR5DirectAccessor(bus, address);
+    access->current_page = this->current_page;
+    return access;
+}
+
+uint8_t DDR5DirectAccessor::at(uint16_t addr)
+{
+    /*-----------------------------------------------------*\
+    | Ensure address is valid                               |
+    \*-----------------------------------------------------*/
+    if(addr >= SPD_DDR5_EEPROM_LENGTH)
+    {
+        return 0xFF;
+    }
+
+    /*-----------------------------------------------------*\
+    | Switch to the page containing address                 |
+    \*-----------------------------------------------------*/
+    set_page(addr >> SPD_DDR5_EEPROM_PAGE_SHIFT);
+
+    /*-----------------------------------------------------*\
+    | Calculate offset                                      |
+    \*-----------------------------------------------------*/
+    uint8_t offset = (uint8_t)(addr & SPD_DDR5_EEPROM_PAGE_MASK) | 0x80;
+
+    /*-----------------------------------------------------*\
+    | Read value at address                                 |
+    \*-----------------------------------------------------*/
+    uint32_t value = bus->i2c_smbus_read_byte_data(address, offset);
+
+    std::this_thread::sleep_for(SPD_IO_DELAY);
+
+    /*-----------------------------------------------------*\
+    | Return value                                          |
+    \*-----------------------------------------------------*/
+    return((uint8_t)value);
+}
+
+void DDR5DirectAccessor::set_page(uint8_t page)
+{
+    /*-----------------------------------------------------*\
+    | Switch page if not already active                     |
+    \*-----------------------------------------------------*/
+    if(current_page != page)
+    {
+        bus->i2c_smbus_write_byte_data(address, SPD_DDR5_MREG_VIRTUAL_PAGE, page);
+        current_page = page;
+        std::this_thread::sleep_for(SPD_IO_DELAY);
+    }
+}
---
+++ SPDAccessor/SPDWrapper.cpp
@@ -0,0 +1,84 @@
+/*---------------------------------------------------------*\
+| SPDWrapper.cpp                                            |
+|                                                           |
+|   Wrapper for DRAM modules using SPD information          |
+|                                                           |
+|   This file is part of the OpenRGB project                |
+|   SPDX-License-Identifier: GPL-2.0-only                   |
+\*---------------------------------------------------------*/
+
+#include "SPDWrapper.h"
+
+SPDWrapper::SPDWrapper(const SPDWrapper &wrapper)
+{
+    if(wrapper.accessor != nullptr)
+    {
+        this->accessor = wrapper.accessor->copy();
+    }
+    this->address = wrapper.address;
+    this->mem_type = wrapper.mem_type;
+}
+
+SPDWrapper::SPDWrapper(const SPDDetector &detector)
+{
+    this->address = detector.spd_address();
+    this->mem_type = detector.memory_type();
+
+    // Allocate a new accessor
+    this->accessor = SPDAccessor::for_memory_type(this->mem_type, detector.smbus(), this->address);
+}
+
+SPDWrapper::~SPDWrapper()
+{
+    delete accessor;
+}
+
+SPDMemoryType SPDWrapper::memory_type()
+{
+    return mem_type;
+}
+
+int SPDWrapper::index()
+{
+    return this->address - 0x50;
+}
+
+uint16_t SPDWrapper::jedec_id()
+{
+    if(accessor == nullptr)
+    {
+        return 0x0000;
+    }
+    return accessor->jedec_id();
+}
+
+/*-------------------------------------------------------------------------*\
+| Helper functions for easier collection handling.                          |
+\*-------------------------------------------------------------------------*/
+
+bool is_jedec_in_slots(std::vector<SPDWrapper> &slots, uint16_t jedec_id)
+{
+    for(SPDWrapper &slot : slots)
+    {
+        if(slot.jedec_id() == jedec_id)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::vector<SPDWrapper*> slots_with_jedec(std::vector<SPDWrapper> &slots, uint16_t jedec_id)
+{
+    std::vector<SPDWrapper*> matching_slots;
+
+    for(SPDWrapper &slot : slots)
+    {
+        if(slot.jedec_id() == jedec_id)
+        {
+            matching_slots.push_back(&slot);
+        }
+    }
+
+    return matching_slots;
+}
