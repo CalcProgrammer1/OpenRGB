CalcProgrammer1/OpenRGB

commit b4d15b95454bbe538ac48d7c21bf3c28f956e7f4

Move wstring to string conversion to StringUtils.cpp and update most HID get serial number functions to use it

Controllers/AsusAuraUSBController/AsusAuraTUFKeyboardController/AsusAuraTUFKeyboardController.cpp
@@ -17,6 +17,7 @@
 #include <string.h>
 #include <vector>
 #include "AsusAuraTUFKeyboardController.h"
+#include "StringUtils.h"
 
 AuraTUFKeyboardController::AuraTUFKeyboardController(hid_device* dev_handle, const char* path, uint16_t pid, unsigned short version)
 {
@@ -70,10 +71,7 @@ std::string AuraTUFKeyboardController::GetSerialString()
         return("");
     }
 
-    std::wstring serial_wstring = serial_string;
-    std::string return_string = clean_serial(serial_wstring);
-
-    return(return_string);
+    return(StringUtils::wstring_to_string(serial_string));
 }
 
 std::string AuraTUFKeyboardController::GetVersion()

commit 74446a3c67fd448e55e361e235d598199352b1c5

Fixed profile loading on Asus ROG Strix Scope II
* Reverted regression introduced with b4d15b954
* Resolves #4632

Controllers/AsusAuraUSBController/AsusAuraTUFKeyboardController/AsusAuraTUFKeyboardController.cpp
@@ -39,36 +39,29 @@ std::string AuraTUFKeyboardController::GetDeviceLocation()
     return("HID: " + location);
 }
 
-std::string clean_serial(const std::wstring& wstr)
+std::string AuraTUFKeyboardController::GetSerialString()
 {
+    wchar_t serial_string[HID_MAX_STR];
+    memset(serial_string, 0, sizeof(serial_string));
+
+    int ret = hid_get_serial_number_string(dev, serial_string, HID_MAX_STR);
+    if(ret != 0)
+    {
+        return("");
+    }
+
     /*-------------------------------------------------------------------------*\
     | Skip non-ASCII, trailing garbage in serial numbers. Required by the       |
     | Scope II 96, whose original firmware outputs garbage, which even differs  |
     | after computer reboots and therefore breaks OpenRGB profile matching.     |
     \*-------------------------------------------------------------------------*/
-    std::string result;
-    for(wchar_t c : wstr)
+    switch(device_pid)
     {
-        // Forbid control chars and anything above final printable low-ASCII.
-        if(c < 32 || c > 126)
-        {
+        case AURA_ROG_STRIX_SCOPE_II_96_WIRELESS_USB_PID:
+            serial_string[12] = L'\0';
             break;
-        }
-
-        result += (char)c;
-    }
-
-    return(result);
-}
-
-std::string AuraTUFKeyboardController::GetSerialString()
-{
-    wchar_t serial_string[128];
-    int ret = hid_get_serial_number_string(dev, serial_string, 128);
-
-    if(ret != 0)
-    {
-        return("");
+        default:
+            break;
     }
 
     return(StringUtils::wstring_to_string(serial_string));

Controllers/AsusAuraUSBController/AsusAuraTUFKeyboardController/AsusAuraTUFKeyboardController.h
@@ -17,6 +17,8 @@
 #include "RGBController.h"
 #include "AsusAuraTUFKeyboardLayouts.h"
 
+#define HID_MAX_STR 128
+
 enum
 {
     AURA_KEYBOARD_MODE_STATIC       =  0,