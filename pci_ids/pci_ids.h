7ac779d45b4f2ce16e968b219e1ce66e30ce78a3
nebula <karlo.tsutskiridze@gmail.com>
2025-03-23 02:44:43 +0400

    support for Palit RTX 5070 Ti GamingPro

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -805,6 +805,7 @@
 #define PALIT_RTX4070TI_GAMEROCK_SUB_DEV                0xF294
 #define PALIT_RTX4080_GAMINGPRO_SUB_DEV                 0xF298
 #define PALIT_RTX4090_GAMEROCK_SUB_DEV                  0xF296
+#define PALIT_RTX5070TI_GAMINGPRO_SUB_DEV               0xF322
 #define PALIT_RTX5090_GAMEROCK_SUB_DEV                  0xF318
 
 /*-----------------------------------------------------*\
