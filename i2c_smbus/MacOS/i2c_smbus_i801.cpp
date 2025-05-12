a6a5792c5f5f349bea5c07ac8a537e0e5b541083
Stephen Horvath <s.horvath@outlook.com.au>
2025-05-13 02:33:05 +1000

    Replace WinRing0 with PawnIO for SMBus and Super-IO access on Windows
    Commits squashed, files cleaned up, and rebased by Adam Honse <calcprogrammer1@gmail.com>

--- i2c_smbus/MacOS/i2c_smbus_i801.cpp
+++ i2c_smbus/MacOS/i2c_smbus_i801.cpp
@@ -1,7 +1,7 @@
 /*---------------------------------------------------------*\
 | i2c_smbus_i801.cpp                                        |
 |                                                           |
-|   i801 SMBUS driver for Windows                           |
+|   i801 SMBUS driver for MacOS                             |
 |                                                           |
 |   Adam Honse (CalcProgrammer1)                29 Jan 2019 |
 |   Portions based on Linux source code                     |
@@ -10,47 +10,22 @@
 |   SPDX-License-Identifier: GPL-2.0-only                   |
 \*---------------------------------------------------------*/
 
+#include "macUSPCIOAccess.h"
+
 #include "Detector.h"
 #include "i2c_smbus_i801.h"
-#include "ResourceManager.h"
-
-#ifdef _WIN32
-#include "OlsApi.h"
-#include "wmi.h"
-#elif _MACOSX_X86_X64
-#include "macUSPCIOAccess.h"
-#endif
-
 #include "LogManager.h"
+#include "ResourceManager.h"
 #include "SettingsManager.h"
 
 using namespace std::chrono_literals;
 
 i2c_smbus_i801::i2c_smbus_i801()
 {
-#ifdef _WIN32
-    json drivers_settings = ResourceManager::get()->GetSettingsManager()->GetSettings("Drivers");
-
-    bool shared_smbus_access = true;
-    if(drivers_settings.contains("shared_smbus_access"))
-    {
-        shared_smbus_access = drivers_settings["shared_smbus_access"].get<bool>();
-    }
-    if(shared_smbus_access)
-    {
-        global_smbus_access_handle = CreateMutexA(NULL, FALSE, GLOBAL_SMBUS_MUTEX_NAME);
-    }
-#endif
 }
 
 i2c_smbus_i801::~i2c_smbus_i801()
 {
-#ifdef _WIN32
-    if(global_smbus_access_handle != NULL)
-    {
-        CloseHandle(global_smbus_access_handle);
-    }
-#endif
 }
 
 /* Return negative errno on error. */
@@ -519,21 +494,7 @@ int i2c_smbus_i801::i801_wait_intr()
 
 s32 i2c_smbus_i801::i2c_smbus_xfer(u8 addr, char read_write, u8 command, int size, i2c_smbus_data* data)
 {
-#ifdef _WIN32
-    if(global_smbus_access_handle != NULL)
-    {
-        WaitForSingleObject(global_smbus_access_handle, INFINITE);
-    }
-#endif
-
     s32 result = i801_access(addr, read_write, command, size, data);
-
-#ifdef _WIN32
-    if(global_smbus_access_handle != NULL)
-    {
-        ReleaseMutex(global_smbus_access_handle);
-    }
-#endif
 
     return result;
 }
@@ -542,103 +503,6 @@ s32 i2c_smbus_i801::i2c_xfer(u8 /*addr*/, char /*read_write*/, int* /*size*/, u8
 {
     return -1;
 }
-
-#ifdef _WIN32
-
-bool i2c_smbus_i801_detect()
-{
-    if(!InitializeOls() || GetDllStatus())
-    {
-        LOG_INFO("WinRing0 is not loaded, i801 I2C bus detection aborted");
-        return(false);
-    }
-
-    i2c_smbus_interface * bus;
-    HRESULT hres;
-    Wmi wmi;
-
-    // Query WMI for Win32_PnPSignedDriver entries with names matching "SMBUS" or "SM BUS"
-    // These devices may be browsed under Device Manager -> System Devices
-    std::vector<QueryObj> q_res_PnPSignedDriver;
-    hres = wmi.query("SELECT * FROM Win32_PnPSignedDriver WHERE Description LIKE '%SMBUS%' OR Description LIKE '%SM BUS%'", q_res_PnPSignedDriver);
-
-    if (hres)
-    {
-        LOG_INFO("WMI query failed, i801 I2C bus detection aborted");
-        return(false);
-    }
-
-    // For each detected SMBus adapter, try enumerating it as either AMD or Intel
-    for (QueryObj &i : q_res_PnPSignedDriver)
-    {
-        // Intel SMBus controllers do show I/O resources in Device Manager
-        // Analysis of many Intel boards has shown that Intel SMBus adapter I/O space varies between boards
-        // We can query Win32_PnPAllocatedResource entries and look up the PCI device ID to find the allocated I/O space
-        // Intel SMBus adapters use the i801 driver
-        if ((i["Manufacturer"].find("Intel") != std::string::npos)
-         || (i["Manufacturer"].find("INTEL") != std::string::npos))
-        {
-            std::string rgx1 = ".+" + q_res_PnPSignedDriver[0]["DeviceID"].substr(4, 33) + ".+";
-
-            AdditionalFilters filters;
-            filters.emplace("Dependent", rgx1);
-            filters.emplace("Antecedent", ".*Port.*");
-
-            std::vector<QueryObj> q_res_PNPAllocatedResource;
-            hres = wmi.query("SELECT * FROM Win32_PnPAllocatedResource", q_res_PNPAllocatedResource, &filters);
-
-            std::regex rgx2(".*StartingAddress=\"(\\d+)\".*");
-            std::smatch matches;
-
-            // Query the StartingAddress for the matching device ID and use it to enumerate the bus
-            if (!q_res_PNPAllocatedResource.empty() && std::regex_search(q_res_PNPAllocatedResource[0]["Antecedent"], matches, rgx2))
-            {
-                unsigned int IORangeStart = std::stoi(matches[1].str());
-
-                std::string pnp_str = i["DeviceID"];
-
-                std::size_t ven_loc = pnp_str.find("VEN_");
-                std::size_t dev_loc = pnp_str.find("DEV_");
-                std::size_t sub_loc = pnp_str.find("SUBSYS_");
-
-                std::string ven_str = pnp_str.substr(ven_loc + 4, 4);
-                std::string dev_str = pnp_str.substr(dev_loc + 4, 4);
-                std::string sbv_str = pnp_str.substr(sub_loc + 11, 4);
-                std::string sbd_str = pnp_str.substr(sub_loc + 7, 4);
-
-                int ven_id = (int)std::stoul(ven_str, nullptr, 16);
-                int dev_id = (int)std::stoul(dev_str, nullptr, 16);
-                int sbv_id = (int)std::stoul(sbv_str, nullptr, 16);
-                int sbd_id = (int)std::stoul(sbd_str, nullptr, 16);
-
-                DWORD pciAddress = FindPciDeviceById(ven_id, dev_id, 0);
-                if(pciAddress == 0xFFFFFFFF)
-                {
-                    continue;
-                }
-
-                uint8_t host_config = (uint8_t)ReadPciConfigWord(pciAddress, SMBHSTCFG);
-                if ((host_config & SMBHSTCFG_HST_EN) == 0)
-                {
-                    continue;
-                }
-
-                bus                         = new i2c_smbus_i801();
-                bus->pci_vendor             = ven_id;
-                bus->pci_device             = dev_id;
-                bus->pci_subsystem_vendor   = sbv_id;
-                bus->pci_subsystem_device   = sbd_id;
-                strcpy(bus->device_name, i["Description"].c_str());
-                ((i2c_smbus_i801 *)bus)->i801_smba = IORangeStart;
-                ResourceManager::get()->RegisterI2CBus(bus);
-            }
-        }
-    }
-
-    return(true);
-}
-
-#elif _MACOSX_X86_X64
 
 bool i2c_smbus_i801_detect()
 {
@@ -674,7 +538,5 @@ bool i2c_smbus_i801_detect()
 
     return(true);
 }
-
-#endif
 
 REGISTER_I2C_BUS_DETECTOR(i2c_smbus_i801_detect);
---
+++ super_io/super_io_pawnio.cpp
@@ -0,0 +1,120 @@
+/*---------------------------------------------------------*\
+| super_io_pawnio.cpp                                       |
+|                                                           |
+|   Functions for interfacing with Super-IO using PawnIO    |
+|                                                           |
+|   Stephen Horvath (Steve-Tech)                05 May 2025 |
+|                                                           |
+|   This file is part of the OpenRGB project                |
+|   SPDX-License-Identifier: GPL-2.0-only                   |
+\*---------------------------------------------------------*/
+
+#include "super_io_pawnio.h"
+
+#include <Windows.h>
+#include "PawnIOLib.h"
+#include "i2c_smbus_pawnio.h"
+
+
+static HANDLE pawnio_handle = NULL;
+
+static int pawnio_chip_type = 0;
+
+static int addr_to_pawnio(int addr)
+{
+    switch (addr)
+    {
+        case 0x2E:
+            return 0;
+        case 0x4E:
+            return 1;
+        default:
+            return -1;
+    }
+}
+
+/******************************************************************************************\
+*                                                                                          *
+*   pawnio_superio_enter                                                                   *
+*                                                                                          *
+*   Put the Super IO chip into Extended Function Mode                                      *
+*                                                                                          *
+\******************************************************************************************/
+
+void superio_enter(int ioreg)
+{
+    HRESULT status;
+    SIZE_T return_size;
+
+    if (pawnio_handle == NULL)
+    {
+        status = i2c_smbus_pawnio::start_pawnio("superio", &pawnio_handle);
+        if (status != S_OK)
+        {
+            // TODO: Figure out how to handle errors
+            return;
+        }
+    }
+
+    if (pawnio_chip_type == 0)
+    {
+        int in_reg = addr_to_pawnio(ioreg);
+        if (in_reg == -1) {
+            return;
+        }
+
+        const SIZE_T in_size = 1;
+        ULONG64 in[in_size] = {(ULONG64)in_reg};
+        const SIZE_T out_size = 1;
+        ULONG64 out[out_size];
+        status = pawnio_execute(pawnio_handle, "ioctl_detect", in, in_size, out, out_size, &return_size);
+        if (status != S_OK || out[0] == 0)
+        {
+            return;
+        }
+        pawnio_chip_type = (int)out[0];
+    }
+
+    pawnio_execute(pawnio_handle, "ioctl_enter", NULL, 0, NULL, 0, &return_size);
+}
+
+
+/******************************************************************************************\
+*                                                                                          *
+*   pawnio_superio_outb                                                                    *
+*                                                                                          *
+*   Write a byte to the Super IO configuration register                                    *
+*                                                                                          *
+\******************************************************************************************/
+
+void superio_outb([[maybe_unused]] int ioreg, int reg, int val)
+{
+    const SIZE_T in_size = 2;
+    ULONG64 in[in_size] = {(ULONG64)reg, (ULONG64)val};
+    SIZE_T return_size;
+    pawnio_execute(pawnio_handle, "ioctl_write", in, in_size, NULL, 0, &return_size);
+}
+
+
+/******************************************************************************************\
+*                                                                                          *
+*   pawnio_superio_inb                                                                     *
+*                                                                                          *
+*   Read a byte from the Super IO configuration register                                   *
+*                                                                                          *
+\******************************************************************************************/
+
+int superio_inb([[maybe_unused]] int ioreg, int reg)
+{
+    const SIZE_T in_size = 1;
+    ULONG64 in[in_size] = {(ULONG64)reg};
+    const SIZE_T out_size = 1;
+    ULONG64 out[out_size];
+    SIZE_T return_size;
+    HRESULT status = pawnio_execute(pawnio_handle, "ioctl_read", in, in_size, out, out_size, &return_size);
+    if (status != S_OK)
+    {
+        return -1;
+    }
+    return (int)out[0];
+}
