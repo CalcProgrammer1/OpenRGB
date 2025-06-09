fc26902d1de894b6b55a6750976d072bd70aac9c
Hepi34 <noah.joss@bluewin.ch>
2025-06-09 18:27:14 +0000

    Implements support for the MSI RTX 5080 SUPRIM SOC

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -776,6 +776,7 @@
 #define MSI_RTX4090_SUPRIM_X_SUB_DEV                    0x5102
 #define MSI_RTX5070TI_GAMING_TRIO_SUB_DEV               0x5315
 #define MSI_RTX5080_GAMING_TRIO_OC_SUB_DEV              0x5315
+#define MSI_RTX5080_SUPRIM_SOC_SUB_DEV                  0x5313
 #define MSI_RTX5090_GAMING_TRIO_SUB_DEV                 0x5303
 #define MSI_RTX5090_SUPRIM_LIQUID_SOC_SUB_DEV           0x5300
 #define MSI_RX6600XT_GAMING_X_SUB_DEV                   0x5020
