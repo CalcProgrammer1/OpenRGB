c0c772b11b2aea0be1166ba1ae821735e8a655e9
Fran <franciscopiracesvega@gmail.com>
2025-07-05 17:44:18 +0000

    Add Palit RTX 5070 GamingPro

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -846,6 +846,7 @@
 #define PALIT_RTX4070TI_GAMEROCK_SUB_DEV                0xF294
 #define PALIT_RTX4080_GAMINGPRO_SUB_DEV                 0xF298
 #define PALIT_RTX4090_GAMEROCK_SUB_DEV                  0xF296
+#define PALIT_RTX5070_GAMINGPRO_SUB_DEV                 0xF324
 #define PALIT_RTX5090_GAMEROCK_SUB_DEV                  0xF318
 
 /*-----------------------------------------------------*\
