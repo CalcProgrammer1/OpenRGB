dee7e1557e8d893b5236d65373d374c5a76270da
Adam Honse <calcprogrammer1@gmail.com>
2025-07-16 00:01:03 -0500

    SDK Version 6 Updates   * SDK Protocol     * Server sends its name to client     * ProfileManager       * Rename existing profile commands       * Add Upload Profile, Download Profile, and Get Active Profile commands     * SettingsManager       * Add Get, Set, and Save Settings commands     * Add zone::active_mode, zone::mode fields for zone-specific modes     * Add NET_PACKET_ID_RGBCONTROLLER_UPDATEZONEMODE packet for updating zone modes     * Add segment::matrix_map to segment packet     * Add NET_PACKET_ID_RGBCONTROLLER_SIGNALUPDATE packet for passing SignalUpdate signal from server to clients   * NetworkServer     * Formatting cleanup     * Use per-controller threads for handling NetworkServer controller-specific packets to avoid delays from controller mutexes   * NetworkClient     * Formatting cleanup   * RGBController     * Clean up and modularize descriptor functions

--- ResourceManager.cpp
+++ ResourceManager.cpp
@@ -199,6 +199,15 @@ ResourceManager::ResourceManager()
     }
 
     /*-----------------------------------------------------*\
+    | Set server name                                       |
+    \*-----------------------------------------------------*/
+    std::string titleString = "OpenRGB ";
+    titleString.append(VERSION_STRING);
+
+    server->SetName(titleString);
+    server->SetSettingsManager(settings_manager);
+
+    /*-----------------------------------------------------*\
     | Enable legacy SDK workaround in server if configured  |
     \*-----------------------------------------------------*/
     if(server_settings.contains("legacy_workaround"))
@@ -1824,6 +1833,7 @@ void ResourceManager::InitCoroutine()
             | detection if the local server was connected   |
             \*---------------------------------------------*/
             auto_connection_active = true;
+            profile_manager->UpdateProfileList();
             DisableDetection();
         }
 
@@ -2149,4 +2159,19 @@ bool ResourceManager::IsAnyDimmDetectorEnabled(json &detector_settings)
         }
     }
     return false;
+}
+
+bool ResourceManager::IsLocalClient()
+{
+    return(auto_connection_active);
+}
+
+NetworkClient* ResourceManager::GetLocalClient()
+{
+    return(auto_connection_client);
+}
+
+unsigned int ResourceManager::GetLocalClientProtocolVersion()
+{
+    return(auto_connection_client->GetProtocolVersion());
 }
