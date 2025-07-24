eeea3b4bb71ac71cfec20cef9e8089479e60bebd
Adam Honse <calcprogrammer1@gmail.com>
2025-07-24 12:08:11 -0500

    Move plugin SDK integration from callback into plugin API and PluginManager

--- ResourceManager.cpp
+++ ResourceManager.cpp
@@ -827,6 +827,11 @@ std::vector<NetworkClient*>& ResourceManager::GetClients()
     return(clients);
 }
 
+PluginManagerInterface* ResourceManager::GetPluginManager()
+{
+    return(plugin_manager);
+}
+
 ProfileManager* ResourceManager::GetProfileManager()
 {
     return(profile_manager);
@@ -1698,6 +1703,12 @@ void ResourceManager::DetectDevicesCoroutine()
         LOG_DIALOG("%s", I2C_ERR_LINUX);
 #endif
     }
+}
+
+void ResourceManager::SetPluginManager(PluginManagerInterface* plugin_manager_ptr)
+{
+    plugin_manager = plugin_manager_ptr;
+    server->SetPluginManager(plugin_manager);
 }
 
 void ResourceManager::StopDeviceDetection()
