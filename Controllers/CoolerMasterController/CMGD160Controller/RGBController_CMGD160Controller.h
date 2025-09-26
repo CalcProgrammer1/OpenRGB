d681fc19d3f43166d31fc9ed9f04bee4cd3302fc
Adam Honse <calcprogrammer1@gmail.com>
2025-09-25 21:38:37 -0500

    Add access mutex to RGBController API

--- Controllers/CoolerMasterController/CMGD160Controller/RGBController_CMGD160Controller.h
+++ Controllers/CoolerMasterController/CMGD160Controller/RGBController_CMGD160Controller.h
@@ -23,8 +23,6 @@ public:
 
     void SetupZones();
 
-    void DeviceResizeZone(int zone, int new_size);
-
     void DeviceUpdateLEDs();
     void DeviceUpdateZoneLEDs(int zone);
     void DeviceUpdateSingleLED(int led);
