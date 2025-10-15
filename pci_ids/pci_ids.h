f573af369f539a700742361ba158151d4ef2e4f2
Liam <liam.schaefer.de@gmail.com>
2025-10-15 18:14:32 +0200

    Add support for Palit GeForce RTX 5070 Ti GamingPro

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -875,6 +875,7 @@
 #define PALIT_RTX4090_GAMEROCK_SUB_DEV                  0xF296
 #define PALIT_RTX5060TI_WHITE_OC_16G_SUB_DEV            0xF330
 #define PALIT_RTX5070_GAMINGPRO_SUB_DEV                 0xF324
+#define PALIT_RTX5070TI_GAMINGPRO_SUB_DEV               0xF322
 #define PALIT_RTX5090_GAMEROCK_SUB_DEV                  0xF318
 
 /*-----------------------------------------------------*\
