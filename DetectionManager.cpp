6e71cd39b9f3fd50e19a682b8e90005985982d2f
Adam Honse <calcprogrammer1@gmail.com>
2026-01-31 22:25:20 -0600

    Add detectors for filtering on IPU only (no VID/PID)

--- DetectionManager.cpp
+++ DetectionManager.cpp
@@ -91,8 +91,10 @@ const hidapi_wrapper default_hidapi_wrapper =
 \*---------------------------------------------------------*/
 bool BasicHIDBlock::compare(hid_device_info* info)
 {
-    return((vid == info->vendor_id)
-        && (pid == info->product_id)
+    return(((vid        == HID_VID_ANY)
+         || (vid == info->vendor_id))
+        && ((pid        == HID_PID_ANY)
+         || (pid == info->product_id))
         && ((usage_page == HID_USAGE_PAGE_ANY)
          || (usage_page == info->usage_page))
         && ((usage      == HID_USAGE_ANY)
@@ -228,7 +230,7 @@ void DetectionManager::RegisterDynamicDetector(std::string name, DynamicDetector
     dynamic_detectors.push_back(detector);
 }
 
-void DetectionManager::RegisterHIDDeviceDetector(std::string name, HIDDeviceDetectorFunction  detector, uint16_t vid, uint16_t pid, int interface, int usage_page, int usage)
+void DetectionManager::RegisterHIDDeviceDetector(std::string name, HIDDeviceDetectorFunction  detector, int vid, int pid, int interface, int usage_page, int usage)
 {
     HIDDeviceDetectorBlock block;
 
@@ -243,7 +245,7 @@ void DetectionManager::RegisterHIDDeviceDetector(std::string name, HIDDeviceDete
     hid_device_detectors.push_back(block);
 }
 
-void DetectionManager::RegisterHIDWrappedDeviceDetector(std::string name, HIDWrappedDeviceDetectorFunction  detector, uint16_t vid, uint16_t pid, int interface, int usage_page, int usage)
+void DetectionManager::RegisterHIDWrappedDeviceDetector(std::string name, HIDWrappedDeviceDetectorFunction  detector, int vid, int pid, int interface, int usage_page, int usage)
 {
     HIDWrappedDeviceDetectorBlock block;
 
