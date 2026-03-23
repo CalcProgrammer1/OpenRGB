4bfe5e733bc1cb1ed85dcf06d8665af2ed6b9443
Adam Honse <calcprogrammer1@gmail.com>
2026-03-23 01:29:55 -0500

    Rework adding segments with ConfigureZone

--- Controllers/LianLiController/LianLiUniHubALController/RGBController_LianLiUniHubAL.cpp
+++ Controllers/LianLiController/LianLiUniHubALController/RGBController_LianLiUniHubAL.cpp
@@ -377,7 +377,8 @@ void RGBController_LianLiUniHubAL::SetupZones()
             zones[channel_idx].flags                = ZONE_FLAG_MANUALLY_CONFIGURABLE_SIZE
                                                     | ZONE_FLAG_MANUALLY_CONFIGURABLE_NAME
                                                     | ZONE_FLAG_MANUALLY_CONFIGURABLE_TYPE
-                                                    | ZONE_FLAG_MANUALLY_CONFIGURABLE_MATRIX_MAP;
+                                                    | ZONE_FLAG_MANUALLY_CONFIGURABLE_MATRIX_MAP
+                                                    | ZONE_FLAG_MANUALLY_CONFIGURABLE_SEGMENTS;
         }
 
         if(!(zones[channel_idx].flags & ZONE_FLAG_MANUALLY_CONFIGURED_NAME))
--- Controllers/LianLiController/LianLiUniHub_AL10Controller/RGBController_LianLiUniHub_AL10.cpp
+++ Controllers/LianLiController/LianLiUniHub_AL10Controller/RGBController_LianLiUniHub_AL10.cpp
@@ -392,7 +392,8 @@ void RGBController_LianLiUniHub_AL10::SetupZones()
             zones[channel_idx].flags                = ZONE_FLAG_MANUALLY_CONFIGURABLE_SIZE
                                                     | ZONE_FLAG_MANUALLY_CONFIGURABLE_NAME
                                                     | ZONE_FLAG_MANUALLY_CONFIGURABLE_TYPE
-                                                    | ZONE_FLAG_MANUALLY_CONFIGURABLE_MATRIX_MAP;
+                                                    | ZONE_FLAG_MANUALLY_CONFIGURABLE_MATRIX_MAP
+                                                    | ZONE_FLAG_MANUALLY_CONFIGURABLE_SEGMENTS;
         }
 
         if(!(zones[channel_idx].flags & ZONE_FLAG_MANUALLY_CONFIGURED_NAME))
