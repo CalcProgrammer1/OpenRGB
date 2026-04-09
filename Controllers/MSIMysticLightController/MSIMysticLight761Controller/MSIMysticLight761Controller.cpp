3416980849c6650399373427b0453871580a9ce3
MissingDLL <missingdll4@gmail.com>
2026-04-09 21:59:30 +0200

    Add MSI MPG B850 EDGE TI WIFI (MS-7E62) to 761 controller

--- Controllers/MSIMysticLightController/MSIMysticLight761Controller/MSIMysticLight761Controller.cpp
+++ Controllers/MSIMysticLightController/MSIMysticLight761Controller/MSIMysticLight761Controller.cpp
@@ -66,7 +66,8 @@ static const std::string board_names[] =
     "MSI MPG X870E EDGE TI WIFI (MS-7E59)",
     "MSI MAG B850 TOMAHAWK MAX WIFI (MS-7E62)",
     "MSI PRO B850M-P WIFI (MS-7E71)",
-    "MSI MAG Z890 TOMAHAWK WIFI (MS-7E32)"
+    "MSI MAG Z890 TOMAHAWK WIFI (MS-7E32)",
+    "MSI MPG B850 EDGE TI WIFI (MS-7E62)"
 };
 
 static const mystic_light_761_config board_configs[] =
@@ -91,6 +92,7 @@ static const mystic_light_761_config board_configs[] =
     { &(board_names[17]), 0,  0,  0, 1, &zone_set1,  MSIMysticLight761Controller::DIRECT_MODE_ZONE_BASED },   // MSI MAG B850 TOMAHAWK MAX WIFI
     { &(board_names[18]), 0,  0,  0, 1, &zone_set1,  MSIMysticLight761Controller::DIRECT_MODE_ZONE_BASED },   // MSI PRO B850M-P WIFI (MS-7E71)
     { &(board_names[19]), 0,  0,  0, 1, &zone_set1,  MSIMysticLight761Controller::DIRECT_MODE_ZONE_BASED },    // MSI MAG Z890 TOMAHAWK WIFI (MS-7E32)
+    { &(board_names[20]), 0,  0,  0, 1, &zone_set1,  MSIMysticLight761Controller::DIRECT_MODE_ZONE_BASED },    // MSI MPG B850 EDGE TI WIFI (MS-7E62)
 };
 
 enum MSI_ZONE setup_map [] =
