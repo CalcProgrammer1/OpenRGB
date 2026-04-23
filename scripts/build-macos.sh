ae9ae18d31fb64e1f31cad07d586f7a24b3302c0
Adam Honse <calcprogrammer1@gmail.com>
2026-04-22 23:21:56 -0500

    Add build-macos.sh script for easier MacOS builds

---
+++ scripts/build-macos.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#-----------------------------------------------------------------------#
+# OpenRGB MacOS Build Script                                            #
+#-----------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------#
+# Setup environment                                                     #
+#-----------------------------------------------------------------------#
+export APPIMAGE_EXTRACT_AND_RUN=1
+
+if   [ "$1" = "qt6" ] && [ "$2" = "arm" ]; then
+    export ARCH=""
+    export BREW_PATH=/opt/homebrew
+    export QT_PATH=bin
+elif [ "$1" = "qt6" ] && [ "$2" = "intel" ]; then
+    export ARCH="arch -x86_64"
+    export BREW_PATH=/usr/local
+    export QT_PATH=bin
+elif [ "$1" = "qt5" ] && [ "$2" = "arm" ]; then
+    export ARCH=""
+    export BREW_PATH=/opt/homebrew
+    export QT_PATH=opt/qt@5/bin
+elif [ "$1" = "qt5" ] && [ "$2" = "intel" ]; then
+    export ARCH="arch -x86_64"
+    export BREW_PATH=/usr/local
+    export QT_PATH=opt/qt@5/bin
+else
+    echo "Invalid arguments, specify qt5 or qt6 and arm or intel"
+    echo "Example: ./build-macos.sh qt6 arm"
+fi
+
+eval $($BREW_PATH/bin/brew shellenv)
+$ARCH $BREW_PATH/$QT_PATH/qmake OpenRGB.pro
+$ARCH make -j$(sysctl -n hw.ncpu)
+$ARCH $BREW_PATH/$QT_PATH/macdeployqt OpenRGB.app -codesign=OpenRGB
