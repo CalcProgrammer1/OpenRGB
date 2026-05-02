18dfe6ca8b1ade4299d6943ae62ce7d75e68164c
w43322 <44689953+w43322@users.noreply.github.com>
2026-05-03 00:48:51 +0800

    add `ASUS TUF GeForce RTX 5060 Gaming OC` support

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -121,6 +121,7 @@
 #define NVIDIA_RTX4080_DEV                              0x2704
 #define NVIDIA_RTX4080S_DEV                             0x2702
 #define NVIDIA_RTX4090_DEV                              0x2684
+#define NVIDIA_RTX5060_DEV                              0x2D05
 #define NVIDIA_RTX5060TI_DEV                            0x2D04
 #define NVIDIA_RTX5070_DEV                              0x2F04
 #define NVIDIA_RTX5070TI_DEV                            0x2C05
@@ -353,6 +354,7 @@
 #define ASUS_ROG_STRIX_RTX_4090_O24G_GAMING_WHITE       0x88C3
 #define ASUS_ROG_STRIX_RTX_4090_O24G_GAMING_WHITE_2     0x88F1
 #define ASUS_ROG_MATRIX_PLATINUM_RTX_4090_24G           0x8934
+#define ASUS_TUF_RTX_5060_O8G_GAMING                    0x8A1A
 #define ASUS_TUF_RTX_5070_O12G_GAMING                   0x89F2
 #define ASUS_TUF_RTX_5070TI_O16G_GAMING                 0x89F4
 #define ASUS_ROG_STRIX_RTX_5070TI_O16G_GAMING_OC        0x8A0D
