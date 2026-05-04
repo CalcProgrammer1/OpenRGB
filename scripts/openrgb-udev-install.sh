a5c084ba3e85bd9a9b0fa0fce5134f38cea921ff
x9cf9w73cv7y <43073889+x9cf9w73cv7y@users.noreply.github.com>
2026-05-04 17:19:09 +0200

    Add support for non-sudo environments in udev script

--- scripts/openrgb-udev-install.sh
+++ scripts/openrgb-udev-install.sh
@@ -3,15 +3,23 @@
 # Download udev rules file
 wget https://openrgb.org/releases/release_0.9/60-openrgb.rules
 
+# Check if sudo is available
+if command -v sudo &>/dev/null; then
+    PRIVILEGED_CMD="sudo"
+else
+    echo "sudo not found. Falling back to run0"
+    PRIVILEGED_CMD="run0 -i"
+fi
+
 # If we don't have write permissions then we're running on an immutable distro
-if sudo test -w /usr/lib/udev/rules.d;
+if PRIVILEGED_CMD test -w /usr/lib/udev/rules.d;
 then
 	# Move udev rules file to udev rules directory
-	sudo mv 60-openrgb.rules /usr/lib/udev/rules.d
+	PRIVILEGED_CMD mv 60-openrgb.rules /usr/lib/udev/rules.d
 else
-	sudo mv 60-openrgb.rules /etc/udev/rules.d
+	PRIVILEGED_CMD 60-openrgb.rules /etc/udev/rules.d
 fi
 
 # Reload the rules
-sudo udevadm control --reload-rules
-sudo udevadm trigger
+PRIVILEGED_CMD udevadm control --reload-rules
+PRIVILEGED_CMD udevadm trigger
