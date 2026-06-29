94333ff2f13c329897254611520f99bd6837473b
Adam Honse <calcprogrammer1@gmail.com>
2026-06-29 14:08:41 -0500

    Fix QChar warning in Qt6 builds from OpenRGBPluginList.cpp

--- qt/OpenRGBPluginsPage/OpenRGBPluginsList.cpp
+++ qt/OpenRGBPluginsPage/OpenRGBPluginsList.cpp
@@ -7,6 +7,7 @@
 |   SPDX-License-Identifier: GPL-2.0-or-later               |
 \*---------------------------------------------------------*/
 
+#include <QChar>
 #include <QMimeData>
 #include <QUrl>
 #include "OpenRGBPluginsList.h"
