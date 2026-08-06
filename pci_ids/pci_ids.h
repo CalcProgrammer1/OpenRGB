6f21dd884c800970c8b10a7140fd63ddf5abae59
Aiden Corbin <aidenkorbs@gmail.com>
2026-08-06 15:02:49 +1000

    Add GALAX GeForce RTX 3070 1-Click OC

--- pci_ids/pci_ids.h
+++ pci_ids/pci_ids.h
@@ -586,6 +586,7 @@
 #define KFA2_RTX_2080_SUPER_EX_OC_SUB_DEV                       0x13A1
 #define KFA2_RTX_2080TI_EX_OC_SUB_DEV                           0x1323
 #define GALAX_RTX_3080_SG_SUB_DEV                               0x1455
+#define GALAX_RTX_3070_1_CLICK_OC_SUB_DEV  			0x146B
 #define GALAX_RTX_5070TI_EX_OC_SUB_DEV                          0x205C
 
 /*-----------------------------------------------------*\
