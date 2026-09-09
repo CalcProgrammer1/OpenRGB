e161314090686aa10478e29c8de6b784618e10f1
brenmous <bmoush@gmail.com>
2026-09-09 23:26:10 +1000

    Add support for MSI B850MPOWER

--- Controllers/MSIMotherboardController/MSIMotherboard761Controller/MSIMotherboard761Controller.cpp
+++ Controllers/MSIMotherboardController/MSIMotherboard761Controller/MSIMotherboard761Controller.cpp
@@ -77,6 +77,7 @@ static const std::string board_names[] =
     "MSI B850 GAMING PLUS WIFI PZ (MS-7E75)",
     "MSI MAG X870E TOMAHAWK MAX WIFI PZ (MS-7E84)",
     "MSI PRO B850M-A WIFI PZ (MS-7E78)",
+    "MSI B850MPOWER (MS-7E83)"
 };
 
 static const mystic_light_761_config board_configs[] =
@@ -111,6 +112,7 @@ static const mystic_light_761_config board_configs[] =
     { &(board_names[27]), 0,  0,  0, 1, &zone_set1,  MSIMotherboard761Controller::DIRECT_MODE_ZONE_BASED },     // MSI B850 GAMING PLUS WIFI PZ (MS-7E75)
     { &(board_names[28]), 0,  0,  0, 1, &zone_set1,  MSIMotherboard761Controller::DIRECT_MODE_ZONE_BASED },     // MSI MAG X870E TOMAHAWK MAX WIFI PZ (MS-7E84)
     { &(board_names[29]), 0,  0,  0, 1, &zone_set1,  MSIMotherboard761Controller::DIRECT_MODE_ZONE_BASED },     // MSI PRO B850M-A WIFI PZ (MS-7E78)
+    { &(board_names[30]), 0,  0,  0, 1, &zone_set1,  MSIMotherboard761Controller::DIRECT_MODE_ZONE_BASED },     // MSI B850MPOWER (MS-7E83)
 };
 
 enum MSI_ZONE setup_map [] =
