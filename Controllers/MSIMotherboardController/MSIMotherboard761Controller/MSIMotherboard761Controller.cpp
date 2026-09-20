f7e0e3d77b5bb87d4716c9457acccff8bb4ff2bb
ubelhj <joeubie@gmail.com>
2026-09-20 17:45:15 +0000

    Add MSI PRO B850-S WIFI6E to supported devices

--- Controllers/MSIMotherboardController/MSIMotherboard761Controller/MSIMotherboard761Controller.cpp
+++ Controllers/MSIMotherboardController/MSIMotherboard761Controller/MSIMotherboard761Controller.cpp
@@ -79,7 +79,8 @@ static const std::string board_names[] =
     "MSI PRO B850M-A WIFI PZ (MS-7E78)",
     "MSI B850MPOWER (MS-7E83)",
     "MSI B840M GAMING WIFI6E (MS-7E76)",
-    "MSI PRO B850M-A WIFI (MS-7E66)"
+    "MSI PRO B850M-A WIFI (MS-7E66)",
+    "MSI PRO B850-S WIFI6E (MS-7E80)"
 };
 
 static const mystic_light_761_config board_configs[] =
@@ -117,6 +118,7 @@ static const mystic_light_761_config board_configs[] =
     { &(board_names[30]), 0,  0,  0, 1, &zone_set1,  MSIMotherboard761Controller::DIRECT_MODE_ZONE_BASED },     // MSI B850MPOWER (MS-7E83)
     { &(board_names[31]), 0,  0,  0, 1, &zone_set1,  MSIMotherboard761Controller::DIRECT_MODE_ZONE_BASED },     // MSI B840M GAMING WIFI6E (MS-7E76)
     { &(board_names[32]), 0,  0,  0, 1, &zone_set1,  MSIMotherboard761Controller::DIRECT_MODE_ZONE_BASED },     // MSI PRO B850M-A WIFI (MS-7E66)
+    { &(board_names[33]), 0,  0,  0, 1, &zone_set1,  MSIMotherboard761Controller::DIRECT_MODE_ZONE_BASED },     // MSI PRO B850-S WIFI6E (MS-7E80)
 };
 
 enum MSI_ZONE setup_map [] =
